e9ed1306d1577c3a97cb844303c2096375cfc34d
Ron Fox <fox@frib.msu.edu>
2025-02-12 07:29:40 -0500

    Issue #230 - Support SIS3316 in VMUSBReadout.
    Initial draft of the CAEN pattern register event segment.

---
+++ main/usb/vmusb/SIS3316Readout/CV977EventSegment.h
@@ -0,0 +1,68 @@
+/**
+ * @file3 CV977EventSegment.h
+ * @brief Header for an event segment to read a V977 pattern register.
+ * 
+ * This is part of the TCL configurable support for D. Bazin's SIS3316 
+ * readout.   The event segment is configurable much like it is
+ * for the VMUSBReadout with a few fewer parameters.
+ * 
+ * Specifically we don't support interrupts.
+ */
+#include "CEventSegment.h"
+#include <string>
+namespace XXUSB {
+    class CConfigurableObject;
+}
+
+class sis_vmusb_interface;    // Because why not use it.
+
+/**
+ *  @class CV977EventSegemnt
+ *     Class definition. Configuration parameters  are:
+ * 
+ * *  -base - base address of the module.
+ * *  -inputmask - the input mask for the module.
+ * *  -outputmask - the output mask for the module.
+ * *  -readmode - singlehit or multihit.
+ * *  -readandcler - bool - true if the register is cleared on read.
+ * *  -pattern - bool - set or don't set the pattern bit in the control register.
+ * *  -gate  - bool - set or dont' set the gate bit in the control register.
+ * *  -ormask - bool do or don't set the ormask in the control register.
+ */
+class CV977EventSegment : public CEventSegment {
+private:
+    std::string name;
+    XXUSB::CConfigurableObject* m_pConfiguration;
+    sis_vmusb_interface*        m_pVME;
+
+        // public canonicals:
+public:
+    CV977EventSegment(const char* name);
+    virtual ~CV977EventSegment();
+
+    // forbidden canonicals:
+
+private:
+    CV977EventSegment(const CV977EventSegment& rhs); // can't copy construct.
+    CV977EventSegment& operator=(const CV977EventSegment& rhs);  // Can't assign.
+
+    int operator==(const CV977EventSegment& rhs) const;   // Can't compare.
+    int operator!=(const CV977EventSegment& rhs) const;   //  ""
+
+    // selectors:
+
+    XXUSB::CConfigurableObject* getConfiguration();
+    std::string getName() const;
+
+    // Virtual entry points.
+
+    virtual void initialize();
+    virtual void disable();
+    virtual size_t read(void* pBuffer, size_t maxwords);
+    
+    // Utilitye methods:
+
+private:
+    void setupConfiguration();
+    
+};
