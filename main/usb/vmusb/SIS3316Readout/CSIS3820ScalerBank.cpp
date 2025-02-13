300940abb9879d1de2b9b39a7e57557861bb081d
Ron Fox <fox@frib.msu.edu>
2025-02-13 10:17:23 -0500

    Issue #230 - Support SIS3316 in VMUSBReadout.
    Draft implementation of the CSIS32820ScalerBank.

---
+++ main/usb/vmusb/SIS3316Readout/CSIS3820ScalerBank.cpp
@@ -0,0 +1,136 @@
+/**
+ * @file CSIS3820ScalerBank.cpp
+ * @brief implementation of a scaler bank bank that holds CSIS3820Scalers.
+ * 
+  * @author Ron Fox <fox at frib dot msu dot edu>
+ *  This software is Copyright by the Board of Trustees of Michigan
+ *  State University (c) Copyright 2025
+*
+*  You may use this software under the terms of the GNU public license
+*   (GPL).  The terms of this license are described at:
+*
+*    http://www.gnu.org/licenses/gpl.txt
+*
+*    Author:
+*            Ron Fox
+*            Facility for Rare Isotop Beams
+*            Michigan State University
+*            East Lansing, MI 48824-1321
+*
+ */
+#include "CSIS3820ScalerBank.h"
+#include "CSIS3820Scaler.h"
+#include <TCLInterpreter.h>
+#include <TCLObject.h>
+#include <memory>
+#include <TCLException.h>
+
+typedef CMDCLASS CSIS3820ScalerBank::CSIS3820Command;    // Mabye better than #defines- for shorthand.
+
+//////////////////////////////////// CSIS3820ScalerBank implementation ///////////////////////////
+/**
+ * constructor
+ *    @param pConfigFilename - name of the configuration file that is interpreted at initialization time.
+ */
+CSIS3820ScalerBank::CSIS3820ScalerBank(const char* pConfigFilename) :
+    m_configFile(pCOnfigFilename) {}
+
+/**
+ *  destructor:
+ *     I don't thikn this will ever get called but maybe we should
+ * delete any modules we now have.
+ */
+CSIS3820ScalerBank::~CSIS3820ScalerBank() {}
+
+
+/**
+ * initialize
+ *    - Delete all of the modules we have now.
+ *    - Make a captive interpreter that has the CSIS3820Command installed.
+ *    - Interpret the configuration file with it.
+ *    - Do the base class initialization.
+ *    - Tear down the interpreter.
+ */
+void
+CSIS3820ScalerBank::initialize() {
+    // Clear the ban of modules:
+
+    while (begin() != end()) {
+        CScaler* p = *begin();   // Should be a pointer.
+        DeleteScaler(p);
+        delete p;                // our scalers are made with new.
+    }
+    // Interpret the configuration file:
+
+    CTCLInterpreter interp;
+    std::unique_ptr<CMDCLASS> pCommand(new CMDCLASS(interp, this));
+    interp.EvalFile(m_configFile);           
+
+    // Now we can initialize:
+
+    CScalerBank::initialize();
+
+    // Destructors wil tear down both the command and the intepreter.
+}
+
+ ///////////////////////////////// CSIS3820SCommand nested class implementation //////////////////////
+
+/** 
+ * constructor
+ *    @param interp - the intepreter we will be registered on.
+ *    @param container - pointer to the scaler bank we will configure.
+ */
+CMDCLASS::CMDCLASS(CTCLInterpeter& interp, CSIS3820ScalerBank* containr) :
+    CTCLObjectProcessor(interp, "sis3820", true), m_pBank(container) {}
+/**
+ * destructor.
+ */
+CMDCLASS::~CMDCLASS() {}
+
+/**
+ *  operator()
+ *    Called in response to the sis3820 command. The form of that command is:
+ * \verbatim
+ *    sis3820 name base-address.
+ * \endverbatim
+ * 
+ * This will create an CSIS3820Scaler object and add it to the scaler bank.
+ * Duplicate module names are not allowed.
+ * 
+ *  @param interp -interpreter running the command.
+ *  @param objv  - the command words encapsulated in CTCLObjects
+ */
+int
+CMDCLASS::operator()(CTCLInterpreter& interp, std::vector<CTCLObject>& objv) {
+    BindAll(interp, objv);      // Bind thewords to the interpreter.
+
+    try {
+        requireExactly(objv, 3, "Incorrect number of command parameters");
+        std::string name = objv[1];
+        int         base = objv[2];
+
+
+        if (exists(name)) {
+            throw std::string("Scaler already exists");
+        }
+        m_pBank->AddScaler(new CSIS3316Scaler(name.c_str(), base));
+    }
+    catch(std::string msg) {
+        throw CTCLException(interp, TCL_ERROR, msg.c_str());   // Our caller handles that.
+    }
+    return TCL_OK;
+}
+
+/**
+ *  exists
+ *    @param name - name of a possibly existing module.k
+ *    @return bool - true if there's already a scaler module named that.
+ */
+bool
+CMDCLASS::exists(const std::string& name) {
+    for (auto p : *m_pBank) {         // I think this works since we scaler banks have the iterator protocol.
+        if(p->name() == name) return true;
+    
+    }
+    return false;
+}
---
+++ main/usb/vmusb/SIS3316Readout/CSIS3820ScalerBank.h
@@ -0,0 +1,72 @@
+/**
+ * @file CSIS3820ScalerBank.h
+ * @brief header for a scaler bank that holds CSIS3820Scalers.
+ * 
+  * @author Ron Fox <fox at frib dot msu dot edu>
+ *  This software is Copyright by the Board of Trustees of Michigan
+ *  State University (c) Copyright 2025
+*
+*  You may use this software under the terms of the GNU public license
+*   (GPL).  The terms of this license are described at:
+*
+*    http://www.gnu.org/licenses/gpl.txt
+*
+*    Author:
+*            Ron Fox
+*            Facility for Rare Isotop Beams
+*            Michigan State University
+*            East Lansing, MI 48824-1321
+*
+ */
+#ifndef CSIS3820SCALERBANK_H
+#define CSIS3820SCALERBANK_H
+
+#include <CScalerBank.h>
+#include <vector>
+#include <TCLObjectProcessor.h>
+class CTCLInterpreter;
+class CTCLObject;
+
+/**
+ * @class CSIS3820ScalerBank
+ *    This is a class that holds CSIS3820Scaler modules and can be
+ * added to the experiment's scaler bank to readout those modules.
+ * This is modeled loosely after the VMUSBReadout configuration in that
+ * associated with this object is a configuration file.  The configuration
+ * file is processed on initialize time to determne the scalers that will
+ * be used for this run.
+ * 
+ * The nexted class CSIS3820Command implements a Tcl extension that 
+ * knows how to create the scalers in the bank.
+ */
+class CSIS3820ScalerBank {
+    class CSIS3820Command : public CTCLObjectProcessor {
+    private:
+        CSIS3820ScalerBank* m_pBank;
+    public:
+        SIS3820Command(CTCLInterpreter& interp, CSIS3820ScalerBank* container);
+        virtual ~CSIS3820Command();
+
+        int operator()(CTCLInterpreter& interp, std::vector<CTCLObject>& objv);
+    };
+private:
+    std::string m_configFile;
+public:
+    CSIS3820ScalerBank(const char* pConfigFilename);
+    virtual ~CSIS3820ScalerBank();
+
+private:
+    CSIS3820ScalerBank(const CSIS3820ScalerBank& rhs);
+    CSIS3820ScalerBank& operator=(const CSIS3820ScalerBank& rhs);
+    int operator==(const CSIS3820ScalerBank& rhs) const;
+    int operator!=(const CSIS3820ScalerBank& rhs) const;
+
+public:
+    virtual void initialize();                // This is the only squirrely thing.
+
+
+};
+
+
+
+#endif
