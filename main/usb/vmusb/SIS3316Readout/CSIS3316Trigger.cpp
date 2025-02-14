61561ccecf3d326a35057e3ce899d1f69179c10c
Ron Fox <fox@frib.msu.edu>
2025-02-14 12:06:36 -0500

    Issue #230 - Support SIS3316 in VMUSBReadout.
    Initial code for the trigger poller.

---
+++ main/usb/vmusb/SIS3316Readout/CSIS3316Trigger.cpp
@@ -0,0 +1,43 @@
+/**
+ * @file CSIS3316Trigger.cpp
+ * @brief implements the trigger based on the readability of an SIS3316 module.
+ * @author Ron Fox <fox at frib dot msu dot edu>
+ * 
+ * 
+ *  This software is Copyright by the Board of Trustees of Michigan
+ *  State University (c) Copyright 2025
+*
+*  You may use this software under the terms of the GNU public license
+*   (GPL).  The terms of this license are described at:
+*
+*    http://www.gnu.org/licenses/gpl.txt
+*
+*    Author:
+*            Ron Fox
+*            Facility for Rare Isotop Beams
+*            Michigan State University
+*            East Lansing, MI 48824-1321
+*
+ * 
+ */
+#include "CSIS3316Trigger.h"
+#include "CConfigurableCompoundEventSegment.h"
+
+/**
+ *  constructor
+ *     @param pSeg - the segment we'll poll
+ */
+CSIS3316Trigger::CSIS3316Trigger(CConfigurableCompoundEventSegment* pSeg) :
+m_pSegment(pSeg) {}
+
+
+/**
+ *  operator()
+ *     Poll the trigger once.
+ * 
+ * @return bool -true if triggered.
+ */
+bool
+CSIS3316Trigger::operator() {
+    return m_pSegment->any3316Readable();
+}
---
+++ main/usb/vmusb/SIS3316Readout/CSIS3316Trigger.h
@@ -0,0 +1,28 @@
+/**
+ * @file CSIS3316Trigger.h
+ * @brief Defines the trigger based on the readability of an SIS3316 module.
+ * @author Ron Fox <fox at frib dot msu dot edu>
+ * 
+ * 
+ *  This software is Copyright by the Board of Trustees of Michigan
+ *  State University (c) Copyright 2025
+*
+*  You may use this software under the terms of the GNU public license
+*   (GPL).  The terms of this license are described at:
+*
+*    http://www.gnu.org/licenses/gpl.txt
+*
+*    Author:
+*            Ron Fox
+*            Facility for Rare Isotop Beams
+*            Michigan State University
+*            East Lansing, MI 48824-1321
+*
+ * 
+ */
+#ifndef CSIS3316TRIGGER_H
+#define  CSIS3316TRIGGER_H
+
+
+
+#endif
