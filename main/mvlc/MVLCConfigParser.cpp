522378fd1f9d2c0fb83beb94d186d770e93eca43
Ron Fox <fox@frib.msu.edu>
2025-04-08 10:43:09 -0400

    Issue #268 - provide mvlc configuration tools
    Add specialized configuration parser that inluces the
    commands registered for generator.

---
+++ main/mvlc/MVLCConfigParser.cpp
@@ -0,0 +1,45 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2025.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Ron Fox 
+             Facility for Rare Isotope4s
+             Michigan State University
+             East Lansing, MI 48824-1321
+
+
+@author Ron Fox <fox at frib dot msu dot edu>
+@brief TCLCOnfigParser implementation.
+*/
+#include "MVLCConfigParser.h"
+// Define the commands to register.
+#include "CStack.h"
+#include "CMarker.h"
+
+/**
+ * constructor:
+ */
+MVLCConfigParser::MVLCConfigParser(const std::string& script):
+    TCLConfigParser(script) {}
+
+/**
+ *  Destructor.
+ */
+MVLCConfigParser::~MVLCConfigParser() {}
+
+/**
+ * addExtensions
+ *    Add the extensions the parser needs:
+ */
+void
+MVLCConfigParser::addExtensions() {
+    CTCLInterpreter& interp(*getInterpreter());
+    addExtension(new CStackCommand(interp, *this));
+    addExtension(new CMarkerCommand(interp, *this));
+}
---
+++ main/mvlc/MVLCConfigParser.h
@@ -0,0 +1,52 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2025.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Ron Fox 
+             Facility for Rare Isotope4s
+             Michigan State University
+             East Lansing, MI 48824-1321
+
+
+@author Ron Fox <fox at frib dot msu dot edu>
+@brief TCLCOnfigParser header with the VMUSB commands registered.alignas
+*/
+
+#ifndef MVLC_MVLCCONFIGPARSER_H
+#define MVLC_MVLCCONFIGPARSER_H
+#include "TCLConfigParser.h"
+
+
+
+/**
+ *  @class MVLCConfigParser
+ *    This class is a TCLConfigParser with all of the commands supported
+ * to generate configuration files for the mvlc.  To add a new
+ * device you need to:
+ * 
+ * 1.  Write a CReadoutHardware for that device that emits the correct VME operations.a
+ * 2.  Write a DeviceCommand that creates a CReadoutModule with an instance of
+ * that device encapsulsted.
+ * 3.  Extend our addExtensions method to register that command.
+ */
+class MVLCConfigParser : public TCLConfigParser {
+public:
+    MVLCConfigParser(const std::string& script);
+    virtual ~MVLCConfigParser();
+private:
+    MVLCConfigParser(const MVLCConfigParser&);
+    MVLCConfigParser& operator=(const MVLCConfigParser&);
+    int operator==(const MVLCConfigParser&);
+    int operator!=(const MVLCConfigParser&);
+protected:
+    virtual void addExtensions();
+};
+
+
+#endif
