2b25106e0e24d98e8b088802e4c87adc5c278f70
Ron Fox <fox@frib.msu.edu>
2025-04-08 08:33:32 -0400

    Issue #268 - provide mvlc configuration tools
    Added CMarker class imported from ../usb/daqdonfig

---
+++ main/mvlc/CMarker.cpp
@@ -0,0 +1,111 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2025.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Ron Fox
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+
+@author Ron Fox <fox at frib dot msu dot edu>
+@brief Header for the driver that generates marker objects.
+      
+*/
+
+
+
+
+#include "CMarker.h"
+
+#include <XXUSBConfigurableObject.h>
+#include <CVMUSBReadoutList.h>
+#include <stdlib.h>
+
+using namespace std;
+
+
+static XXUSB::CConfigurableObject::limit valueLow(0);
+static XXUSB::CConfigurableObject::limit valueHigh(0xffffffff);  // mvlc 32 bit markers
+static XXUSB::CConfigurableObject::Limits valueLimits(valueLow, valueHigh);
+
+
+///////////////////////////////////////////////////////////////////////////////////////
+////////////////////////// Canonical class/object operations /////////////////////////
+//////////////////////////////////////////////////////////////////////////////////////
+
+/** 
+ * constructor:
+ */
+CMarker::CMarker() :
+  m_pConfiguration(0)
+{}
+
+/**
+ * destructor
+ */
+CMarker::~CMarker() {}
+
+/////////////////////////////////////////////////////////////////////////////////
+//////////////////////// Overridable operations /////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////////
+
+/*!
+    Attach the module to its configuration by  storing the config reference and
+    adding the -value parameter to the config.
+    \param CReadoutModule& configuration
+
+*/
+void
+CMarker::onAttach(XXUSB::CConfigurableObject& configuration)
+{
+  m_pConfiguration = &configuration;
+
+  m_pConfiguration->addParameter(
+    "-value", XXUSB::CConfigurableObject::isInteger, &valueLimits, "0"
+  );
+
+}
+/*!
+   The device does not need to be initialized.
+*/
+void 
+CMarker::Initialize(CVMUSB& controller)
+{}
+
+/*!
+    The module is added to the readout list by fetching the value parameter as a 16 bit integer
+    and adding the marker command to the readout list.
+*/
+void
+CMarker::addReadoutList(CVMUSBReadoutList& list)
+{
+  unsigned int value = getIntegerParameter("-value");
+
+  list.addMarker(static_cast<uint16_t>(value));
+}
+
+
+/////////////////////////////////////////////////////////////////////
+//////////////////// Private utility functions //////////////////////
+/////////////////////////////////////////////////////////////////////
+
+// Return the value of an integer parameter.
+// Parameters:
+//    std::string name - name of the parameter.
+// Returns:
+//    value
+// Throws a string exception (from cget) if there is no such parameter.
+// caller is responsible for ensuring the parameter is an int.
+//
+unsigned int
+CMarker::getIntegerParameter(string name)
+{
+  return m_pConfiguration->getUnsignedParameter(name);
+
+}
---
+++ main/mvlc/CMarker.h
@@ -0,0 +1,86 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2025.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+     Author:
+             Ron Fox
+	     NSCL
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+
+@author Ron Fox <fox at frib dot msu dot edu>
+@brief Header for the driver that generates marker objects.
+*/
+
+
+#ifndef MVLC__CMARKER_H
+#define MVLC__CMARKER_H
+
+
+#include "CReadoutHardware.h"
+
+#include <stdint.h>
+#include <string>
+#include <vector>
+
+
+// Forward class definitions:
+
+class CReadoutModule;
+class CVMUSB;
+class CVMUSBReadoutList;
+namespace XXUSB {
+  class CConfigurableObject;
+}
+
+/*!
+  The CMarker class is a module that inserts a marker word into the output
+  data stream.  The marker word is a 16 bit datum. You can insert a 32 bit
+  marker by inserting two of these with the low order 16 bits first.
+
+  Configuration parameter is:
+
+\verbatim
+Parameter      Value type              value meaning
+-value         integer [0-0xffff]       The marker word to insert. This must be provided.
+                                        (well it actually defaults to 0).
+
+
+*/
+
+class CMarker : public CReadoutHardware
+{
+private:
+  XXUSB::CConfigurableObject*    m_pConfiguration;
+public:
+  // Class canonicals:
+
+  CMarker();
+  virtual ~CMarker();
+
+  // Disallowed canonicals.
+private:
+  CMarker(const CMarker& CMarker);
+  CMarker& operator=(const CMarker& rhs);
+  int operator==(const CMarker& rhs);
+  int operator!=(const CMarker& rhs);
+
+
+public:
+  virtual void onAttach(XXUSB::CConfigurableObject& configuration);
+  virtual void Initialize(CVMUSB& controller);
+  virtual void addReadoutList(CVMUSBReadoutList& list);
+
+private:
+  unsigned int getIntegerParameter(std::string name);
+
+
+};
+
+
+#endif
