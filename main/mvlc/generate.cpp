353c01aa267841ec57447c4510bcb1bcc97852a0
Ron Fox <fox@frib.msu.edu>
2025-04-09 07:53:30 -0400

    Issue #268 - provide mvlc configuration tools
    Instead of dumping the config, instantiate a generator
    and call it, catching exceptions as errors.
    
    Note: yaml-cpp throws std::exception derived exceptions.

--- main/mvlc/generate.cpp
+++ main/mvlc/generate.cpp
@@ -31,57 +31,19 @@
 #include "CStack.h"
 #include "CVMUSB.h"
 #include "CVMUSBReadoutList.h"
+#include "MVLCGenerator.h"
+#include <stdexcept>
 
-
-static void dump(const std::vector<std::string>& ops) {
-    std::cout << "BEGIN:\n";
-    for (auto op : ops) {
-        std::cout << " -- " << op << std::endl;
-    }
-    std::cout << "END\n";
-}
 
 static void generateYaml(TCLConfigParser& parser, std::string outfile) {
-    CStack* event = parser.getEventStack();
-    CStack* scaler = parser.getScalerStack();
-
-    if (event) {
-        CVMUSB controller;
-        CVMUSBReadoutList list;
-        std::cout <<  "--- event stack initialiation\n";
-        event->Initialize(controller);
-        auto initops = controller.getRecordedOperations();
-        controller.clearRecordedOperations();
-        dump(initops);
-        event->addReadoutList(list);
-        auto readops = list.dumpForMvlc();
-        std::cout << "-- event stack readout\n";
-        dump(readops);
-        event->onEndRun(controller);
-        std::cout << "-- event stack end run \n";
-        auto endops = controller.getRecordedOperations();
-        dump(endops);
+    MVLCGenerate generator(outfile, &parser);
+    try {
+        generator.generate();    
     }
-    if (scaler) {
-        CVMUSB controller;
-        CVMUSBReadoutList list;
-        std::cout <<  "--- scaler stack initialiation\n";
-        scaler->Initialize(controller);
-        auto initops = controller.getRecordedOperations();
-        controller.clearRecordedOperations();
-        dump(initops);
-        scaler->addReadoutList(list);
-        auto readops = list.dumpForMvlc();
-        std::cout << "-- scaler stack readout\n";
-        dump(readops);
-        scaler->onEndRun(controller);
-        std::cout << "-- scaler stack end run \n";
-        auto endops = controller.getRecordedOperations();
-        dump(endops);
+    catch(std::exception& e) {
+        std::cerr << "Failed to generate output file: "<< e.what() << std::endl;
+        exit(EXIT_FAILURE);
     }
-
-    
-
 }
 
 int main(int argc, char** argv) {
