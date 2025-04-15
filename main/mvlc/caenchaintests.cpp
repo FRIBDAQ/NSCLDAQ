2adb32f2d0c28e3e096a6a42a81b1fdf3e3d1453
Ron Fox <fox@frib.msu.edu>
2025-04-15 10:42:26 -0400

    Issue #268 - provide mvlc configuration tools
    Tested recoded chain validation.

--- main/mvlc/caenchaintests.cpp
+++ main/mvlc/caenchaintests.cpp
@@ -27,6 +27,7 @@
 #include "MVLCConfigParser.h"
 #include "CStack.h"
 #include <TCLInterpreter.h>
+#include <TCLException.h>
 #include <tcl.h>
 #include <string>
 #include <stdlib.h>
@@ -39,10 +40,20 @@
 class CAENChainTests : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(CAENChainTests);
     CPPUNIT_TEST(command_1);     // caenchain was registered.
+    
+    CPPUNIT_TEST(chain_1);       // Two modules both ok.
+    CPPUNIT_TEST(chain_2);       // three modules all ok.
+    CPPUNIT_TEST(chain_3);       // THree modules one nonexistent.
+    CPPUNIT_TEST(chain_4);       // THre modules one, not a C785.
+
     CPPUNIT_TEST_SUITE_END();
 
 protected:
     void command_1();
+    void chain_1();
+    void chain_2();
+    void chain_3();
+    void chain_4();
 
 public:
     // The usual setup/teardown for this sort of test
@@ -78,4 +89,66 @@ CAENChainTests::command_1() {
     auto token = Tcl_FindCommand(interp, "caenchain", nullptr, TCL_GLOBAL_ONLY);
 
     CPPUNIT_ASSERT(token);
+}
+////////////////////// tests for validating chains.
+
+void
+CAENChainTests::chain_1() {
+    // Make the script:
+
+    {
+        std::ofstream script(m_filename);
+        script << "adc create one -base 0x10000000 -geo 2\n";
+        script << "adc create two -base 0x20000000 -geo 3\n";
+        script << "adc create three -base 0x40000000 -geo 4\n";
+        script << "caenchain create chain -base 0xff000000 -modules [list one two]\n"; // minimal chain.
+        script << "stack create event -trigger nim1 -modules chain\n";
+    }
+    CPPUNIT_ASSERT_NO_THROW(
+        (*m_parser)()
+    );
+
+}
+
+void
+CAENChainTests::chain_2() {
+    {
+        std::ofstream script(m_filename);
+        script << "adc create one -base 0x10000000 -geo 2\n";
+        script << "adc create two -base 0x20000000 -geo 3\n";
+        script << "adc create three -base 0x40000000 -geo 4\n";
+        script << "caenchain create chain -base 0xff000000 -modules [list one two three]\n"; // middle module.
+        script << "stack create event -trigger nim1 -modules chain\n";
+    }
+    CPPUNIT_ASSERT_NO_THROW(
+        (*m_parser)()
+    );
+}
+void
+CAENChainTests::chain_3() {
+    {
+        std::ofstream script(m_filename);
+        script << "adc create one -base 0x10000000 -geo 2\n";
+        script << "adc create two -base 0x20000000 -geo 3\n";
+        script << "adc create three -base 0x40000000 -geo 4\n";
+        script << "caenchain create chain -base 0xff000000 -modules [list one nosuch]\n"; // minimal chain.
+        script << "stack create event -trigger nim1 -modules chain\n";
+    }
+    CPPUNIT_ASSERT_THROW(
+        (*m_parser)(), CTCLException
+    );
+}
+void
+CAENChainTests::chain_4() {
+    {
+        std::ofstream script(m_filename);
+        script << "adc create one -base 0x10000000 -geo 2\n";
+        script << "adc create two -base 0x20000000 -geo 3\n";
+        script << "madc create three -base 0x40000000 -geo 4\n";
+        script << "caenchain create chain -base 0xff000000 -modules [list one two three]\n"; // three isn't a valid module.
+        script << "stack create event -trigger nim1 -modules chain\n";
+    }
+    CPPUNIT_ASSERT_THROW(
+        (*m_parser)(), CTCLException
+    );
 }
