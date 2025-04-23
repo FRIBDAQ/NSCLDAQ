444dd953af25d967a29a7b7630774dc3a4d55544
Ron Fox <fox@frib.msu.edu>
2025-04-23 07:24:47 -0400

    Issue #268 - provide mvlc configuration tools
    Completed basic testing of the CNADC2530 module.

--- main/mvlc/hytectests.cpp
+++ main/mvlc/hytectests.cpp
@@ -21,6 +21,7 @@
 
 
 #include "CVMUSBReadoutList.h"
+#include "CReadoutModule.h"
 #include "CVMUSB.h"
 #include "CMADC32.h"
 #include "CNADC2530.h"
@@ -39,10 +40,16 @@
 class HytecTests : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(HytecTests);
     CPPUNIT_TEST(command_1);
+    CPPUNIT_TEST(create_1);
+    CPPUNIT_TEST(init_1);
+    CPPUNIT_TEST(readout_1);
     CPPUNIT_TEST_SUITE_END();
 
 protected:
     void command_1();     // The hytec command is registered.
+    void create_1();     // Can make device via command.
+    void init_1();        // 12 initialization operations.
+    void readout_1();   // Readout list checks.
 public:
     void setUp() {
         // Make a temp script file:
@@ -74,4 +81,79 @@ HytecTests::command_1() {
     Tcl_Interp* pInterp = m_parser->getInterpreter()->getInterpreter();
     auto token = Tcl_FindCommand(pInterp, "hytec", nullptr, TCL_GLOBAL_ONLY);
     CPPUNIT_ASSERT(token);
+}
+
+// 'hytec' can create devices.
+
+void
+HytecTests::create_1() {
+    {
+        std::ofstream script(m_filename);
+        script << "hytec create nadc -csr 0x120000 -memory 0xff000000\n";   // a24 regs, a32 memory.
+        script << "stack create evetn -trigger nim1 -modules nadc\n";
+    }
+    CPPUNIT_ASSERT_NO_THROW(
+        (*m_parser)()
+    );
+
+
+    // SHould be able to find the module and its driver is a CNADC2530 instance:
+
+    auto pModule = m_parser->findDevice("nadc");
+    CPPUNIT_ASSERT(pModule);
+
+    auto pDevice = pModule->getDriver();
+    CPPUNIT_ASSERT(pDevice);
+    CPPUNIT_ASSERT(dynamic_cast<CNADC2530*>(pDevice));
+}
+// initialization.
+
+void
+HytecTests::init_1(){
+    {
+        std::ofstream script(m_filename);
+        script << "hytec create nadc -csr 0x120000 -memory 0xff000000\n";   // a24 regs, a32 memory.
+        script << "stack create evetn -trigger nim1 -modules nadc\n";
+    }
+    CPPUNIT_ASSERT_NO_THROW(
+        (*m_parser)()
+    );
+
+
+    CStack* pStack = m_parser->getEventStack();
+    CPPUNIT_ASSERT(pStack);                      // We did make one.
+
+    CVMUSB controller;
+    pStack->Initialize(controller);
+
+    auto ops = controller.getRecordedOperations();
+
+    CPPUNIT_ASSERT_EQUAL(size_t(12), ops.size());
+}
+
+// Readout list.
+
+void
+HytecTests::readout_1() {
+    {
+        std::ofstream script(m_filename);
+        script << "hytec create nadc -csr 0x120000 -memory 0xff000000\n";   // a24 regs, a32 memory.
+        script << "stack create evetn -trigger nim1 -modules nadc\n";
+    }
+    CPPUNIT_ASSERT_NO_THROW(
+        (*m_parser)()
+    );
+
+
+    CStack* pStack = m_parser->getEventStack();
+    CPPUNIT_ASSERT(pStack);                      // We did make one.
+
+    CVMUSBReadoutList list;
+    pStack->addReadoutList(list);
+
+    auto ops = list.dumpForMvlc();
+
+    // LIst size:  Note masked count read is 2 list elements.
+
+    CPPUNIT_ASSERT_EQUAL(size_t(9), ops.size());
 }
