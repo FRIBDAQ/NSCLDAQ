f8c13a1c9b6ac670ab297b12cb8e3bc20d83fe77
Ron Fox <fox@frib.msu.edu>
2025-04-23 08:52:19 -0400

    Issue #268 - provide mvlc configuration tools
    Completed successful tetsing of CV977 and V977Command.

--- main/mvlc/v977tests.cpp
+++ main/mvlc/v977tests.cpp
@@ -39,10 +39,16 @@
 class V977Tests : public CppUnit::TestFixture {
     CPPUNIT_TEST_SUITE(V977Tests);
     CPPUNIT_TEST(command_1);   // "v977" is registered.
+    CPPUNIT_TEST(create_1);    // Can use "v977" to create  a module.
+    CPPUNIT_TEST(init_1);     // number of initialization ops is correct.
+    CPPUNIT_TEST(read_1);    // Readout list is correctd.
     CPPUNIT_TEST_SUITE_END();
 
 protected:
     void command_1();
+    void create_1();
+    void init_1();
+    void read_1();
 
 public:
     void setUp() {
@@ -73,4 +79,62 @@ V977Tests::command_1() {
     Tcl_Interp* pInterp = m_parser->getInterpreter()->getInterpreter();
     auto token  = Tcl_FindCommand(pInterp, "v977", nullptr, TCL_GLOBAL_ONLY);
     CPPUNIT_ASSERT(token);
+}
+
+void V977Tests::create_1() {
+    {
+        std::ofstream script(m_filename);
+        script << "v977 create input -base 0x80000000\n";
+        script << "stack create event -trigger nim1 -modules input\n";
+    }
+    CPPUNIT_ASSERT_NO_THROW(
+        (*m_parser)()
+    );
+
+    auto pModule = m_parser->findDevice("input");
+    CPPUNIT_ASSERT(pModule);
+    auto pDriver = pModule->getDriver();
+    CPPUNIT_ASSERT(pDriver);
+    CPPUNIT_ASSERT(dynamic_cast<CV977*>(pDriver));
+}
+
+void V977Tests::init_1() {
+    {
+        std::ofstream script(m_filename);
+        script << "v977 create input -base 0x80000000\n";
+        script << "stack create event -trigger nim1 -modules input\n";
+    }
+    CPPUNIT_ASSERT_NO_THROW(
+        (*m_parser)()
+    );
+    CStack* pEvent = m_parser->getEventStack();
+    CVMUSB controller;
+    pEvent->Initialize(controller);
+
+    auto ops = controller.getRecordedOperations();
+    CPPUNIT_ASSERT_EQUAL(size_t(6), ops.size());
+}
+
+// Note that by default it is singlehit read and clear.
+
+void V977Tests::read_1() {
+    {
+        std::ofstream script(m_filename);
+        script << "v977 create input -base 0x80000000\n";
+        script << "stack create event -trigger nim1 -modules input\n";
+    }
+    CPPUNIT_ASSERT_NO_THROW(
+        (*m_parser)()
+    );
+    CStack* pEvent = m_parser->getEventStack();
+    CVMUSBReadoutList list;
+    pEvent->addReadoutList(list);
+
+    auto ops = list.dumpForMvlc();
+    CPPUNIT_ASSERT_EQUAL(size_t(1), ops.size());
+
+    CPPUNIT_ASSERT_EQUAL(
+        std::string("vme_read_mem 0x9 d16 0x80000016"),
+        ops.at(0)
+    );
 }
