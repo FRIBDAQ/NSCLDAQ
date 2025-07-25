6d9d57faffdea360a6c02e737ff41cf29c42cfac
Ron Fox <fox@frib.msu.edu>
2025-07-25 11:36:04 -0400

    Issue #314 - Provide a direct XLM72 loader with MVLC
    Decode the --base | --slot parameter and verify that one of them
    is required.

--- main/mvlc/xlmload.cpp
+++ main/mvlc/xlmload.cpp
@@ -5,10 +5,22 @@
  */
 #include "xlm_options.h"
 #include <stdlib.h>
+#include <stdint.h>
+#include <iostream>
 int main(int argc, char** argv) {
     gengetopt_args_info args;
     if (cmdline_parser(argc, argv, &args)) {
         exit(EXIT_FAILURE);
     }
+    // Figure out the base address of the XLM based on the options:
+
+    uint32_t xlmBase = 0;
+    if (args.slot_given) {
+        xlmBase = args.slot_arg << 27;     // base adress given the slot.
+    } else {
+        xlmBase = args.base_arg;
+    }
+    std::cerr << "Loading XLM at : 0x" << std::hex << xlmBase << std::dec << std::endl;
+
     return EXIT_SUCCESS;
 }
