8644a086aaa6b6949f0eba301d0cf0e678e2f4c8
Ron Fox <fox@frib.msu.edu>
2025-07-25 12:11:19 -0400

    Issue #314 - Provide a direct XLM72 loader with MVLC
    Properly decode the MVLC controller connection option(s),
    create the correct MVLC object and attempt to connect to it
    with appropriate error recovery.

--- main/mvlc/xlmload.cpp
+++ main/mvlc/xlmload.cpp
@@ -7,6 +7,11 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <iostream>
+#include <mesytec-mvlc/mesytec-mvlc.h>
+#include <string>
+
+using namespace mesytec::mvlc;
+
 int main(int argc, char** argv) {
     gengetopt_args_info args;
     if (cmdline_parser(argc, argv, &args)) {
@@ -21,6 +26,31 @@ int main(int argc, char** argv) {
         xlmBase = args.base_arg;
     }
     std::cerr << "Loading XLM at : 0x" << std::hex << xlmBase << std::dec << std::endl;
+    
+    // Creaete and connect the MVLC based on the connection argument given:
+    // and connect to the controller:
 
+    MVLC interface;
+    if (args.host_given) {
+        std::cerr << "Connect ethernet to " << args.host_arg << std::endl;
+        interface = make_mvlc_eth(std::string(args.host_arg));
+    } else if (args.usb_given) {
+        std::cerr << "Connect USB  to index " << args.usb_arg << std::endl;
+        if (args.usb_arg > 0) {
+            interface = make_mvlc_usb(args.usb_arg);
+        } else {
+            interface = make_mvlc_usb();
+        }
+    } else if (args.serial_given) {
+        std::cerr << "Connect USB to serial number: " << args.serial_arg << std::endl;
+        interface = make_mvlc_usb(std::string(args.serial_arg));
+    }
+    auto ec = interface.connect();
+    if (ec) {
+        std::cerr << "Unable to connect to the MVLC: " << ec.message() << std::endl;
+        exit(EXIT_FAILURE);
+    }
+
+    
     return EXIT_SUCCESS;
 }
