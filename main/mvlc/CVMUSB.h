e06095c5be8bdea89280f22b80a3209a3bfd49a8
Ron Fox <fox@frib.msu.edu>
2025-07-28 09:59:58 -0400

    Issue #314 - Provide a direct XLM72 loader with MVLC
    * Changes to CVMUSB  to support making it a base class for:
    * CMVLCDirect - wrote and good compile to provide a CVMUSB 'controller'
      that can be used connected to an MVLC.

--- main/mvlc/CVMUSB.h
+++ main/mvlc/CVMUSB.h
@@ -47,7 +47,7 @@ public:
     std::vector<std::string> getRecordedOperations();
     void clearRecordedOperations();
 
-    int executeList(CVMUSBReadoutList& list, void* pReadBuffer, size_t readBufferSize, size_t* bytesRead);
-    std::vector<uint8_t> executeList(CVMUSBReadoutList& list, int maxBytes);
+    virtual int executeList(CVMUSBReadoutList& list, void* pReadBuffer, size_t readBufferSize, size_t* bytesRead);
+    std::vector<uint8_t> executeList(CVMUSBReadoutList& list, int maxBytes); // In terms of the virtual one.
 };
 #endif
--- main/mvlc/xlmload.cpp
+++ main/mvlc/xlmload.cpp
@@ -61,6 +61,6 @@ int main(int argc, char** argv) {
         exit(EXIT_FAILURE);
     }
 
-    
+        
     return EXIT_SUCCESS;
 }
