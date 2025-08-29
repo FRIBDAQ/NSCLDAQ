2a5762107149ce7b95d7dc6b070615fb6159da19
Ron Fox <fox@frib.msu.edu>
2025-08-29 08:04:33 -0400

    Issue #348 - provide ring buffer status monitor
    Tested rate computation with multiple updates to the same run.

--- main/rust_tools/ring_monitor/src/main.rs
+++ main/rust_tools/ring_monitor/src/main.rs
@@ -108,6 +108,7 @@ impl StatisticsAndRates {
             panic!("{}", msg);
         }
 
+
         // Need differences from last and now to get rates:
 
         let dt : f64 = info.interval.as_secs_f64();
@@ -648,6 +649,37 @@ mod sandr_tests {
                 cum_statistics : RingBufferStatistics {
                     name : String::from("test"), 
                     bytes: 150, events: 7, bytes_this_run: 50, events_this_run: 2
+
+                },
+                byte_rate: 50.0, event_rate: 2.0, byte_per_run_rate : 50.0, evts_per_run_rate: 2.0
+            },
+            stats
+        )
+
+    }
+    #[test]
+    fn update_3() {
+        // Two updates that change rate but no runs:
+
+        let mut stats = StatisticsAndRates::new("test");
+        let interval = Duration::from_secs(1);
+        let mut incr_stat = RingBufferStatistics::new("test");
+        incr_stat.count_bytes(100)
+            .count_events(5);
+        let msg = UpdateMessage::new(interval, &incr_stat);
+
+        stats.update(&msg);        // Establishes baseline counts:
+
+        incr_stat.count_bytes(50)
+            .count_events(2);             // Should make stats thing this is a new run.
+        let msg = UpdateMessage::new(interval, &incr_stat);
+        stats.update(&msg);
+        
+        assert_eq!(
+            StatisticsAndRates {
+                cum_statistics : RingBufferStatistics {
+                    name : String::from("test"), 
+                    bytes: 150, events: 7, bytes_this_run: 150, events_this_run: 7
 
                 },
                 byte_rate: 50.0, event_rate: 2.0, byte_per_run_rate : 50.0, evts_per_run_rate: 2.0
