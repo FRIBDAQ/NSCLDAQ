5ee945dc1f39b357e1c17ebcf4c826f6c201e5e2
Ron Fox <fox@frib.msu.edu>
2025-09-02 10:13:09 -0400

    Issue # 348 - provide statistics viewer
    *  Add code to get the port of the ring monitor.
    *  Add rough outline of code to update table.

--- main/utilities/statsview/statsview.py
+++ main/utilities/statsview/statsview.py
@@ -11,15 +11,65 @@
 # If host is supplied, it indcicates which host's ring monitor
 # will be contacted.  If not supplied, locahost will be used.
 #
+# TODO: allow more than one host and make the table view 
+#     show rings from all the requested hosts.
+#
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import QApplication, QWidget, QTableView
+from PyQt5.QtCore import    QAbstractTableModel
+from nscldaq.portmanager.PortManager import PortManager
+
+SERVICENAME="RING_MONITOR"
+UPDATE_MILLISECONDS=5000   # I don't think the server updates faster.
+
+##
+# getServerPort
+#    Return the port the Ring monitor is on.
+#  @param host - the host to connect to.
+#  @return port number (16 bit integer)
+#  @retval None - can't get a port.
+
+def getServerPort(host) :
+    pm = PortManager(host)
+    info = pm.find(service=SERVICENAME)
+    if len(info) > 0:
+        return info[0]['port']
+    else:
+        return None
+
+def getStatistics(host, port):
+    pass
+
+def populateModel(data):
+    pass
 
 if __name__ == '__main__':
+    host = "localhost"
+    if len(sys.argv) > 1:
+        host = sys.argv[1]
+    print("Getting statistics from host: ", host)
+    
+    #  Get the ring monitor port:
+    
+    port = getServerPort(host)
+    if port is None:
+        print('The ring monitor server is not running in ', host)
+    
+    print("Ring monitor is running  on", port)
+    
     # 1. Create an instance of QApplication
     app = QApplication(sys.argv)
-
     # 2. Create a QWidget (our window)
-    window = QWidget()
+    window = QTableView()
+    window.setShowGrid(True)
+    model = QAbstractTableModel(window)
+    
+    #  Get the initial data, populate the model, and 
+    #  Schedule updates.
+    
+    data = getStatistics(host, port)
+    populateModel(data)
+    
 
     # Optional: Set window properties
     window.setWindowTitle('Minimal PyQt Window')
@@ -29,5 +79,4 @@ if __name__ == '__main__':
     window.show()
 
     # 4. Start the application's event loop
-    sys.exit(app.exec_())
-    
+    sys.exit(app.exec())
