545b84f3eedf9146148bc44efce0409fe22cb524
Ron Fox <fox@frib.msu.edu>
2025-11-26 10:49:35 -0500

    Issue #398 Support s800 readout.
    Add the s800trigger command to the configurator.

--- main/usb/vmusb/daqconfig/CConfiguration.cpp
+++ main/usb/vmusb/daqconfig/CConfiguration.cpp
@@ -43,6 +43,7 @@
 #include "CCBD8210CrateController.h"
 #include "CCBD8210ReadoutList.h"
 #include "CSIS3316.h"
+#include "CS800TriggerNew.h"
 #include "CUserCommand.h"
 
 #include <CMASE.h>
@@ -134,6 +135,7 @@ CConfiguration::CConfiguration() :
   m_Commands.push_back(new CUserCommand(
        *m_pInterp, *this, "sis3316", new CSIS3316)
   );
+  m_Commands.push_back(new CUserCommand( *m_pInterp, *this, "s800trigger", new CS800TriggerNew));
   
 
   // Add hybrid drivers
