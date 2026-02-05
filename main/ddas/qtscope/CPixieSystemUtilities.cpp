f3db2c2472660eed61aa6cf811a31fddd28533c2
Chester, Aaron <chestera@msu.edu>
2026-02-05 15:03:22 -0500

    formatting c++ stuff

--- main/ddas/qtscope/CPixieSystemUtilities.cpp
+++ main/ddas/qtscope/CPixieSystemUtilities.cpp
@@ -5,253 +5,233 @@
 
 #include "CPixieSystemUtilities.h"
 
-#include <sstream>
 #include <iostream>
+#include <sstream>
 
 #include <config.h>
 #include <config_pixie16api.h>
 
-#include <SystemBooter.h>
 #include <CXIAException.h>
+#include <SystemBooter.h>
 
 using namespace DAQ::DDAS;
 namespace HR = DAQ::DDAS::HardwareRegistry;
 
 /**
  * @details
- * Default: boot in online mode and read the settings file specified in 
+ * Default: boot in online mode and read the settings file specified in
  * cfgPixie16.txt.
  */
-CPixieSystemUtilities::CPixieSystemUtilities() :
-    m_bootMode(0), m_booted(false), m_ovrSetFile(false), m_numModules(0)
-{}
+CPixieSystemUtilities::CPixieSystemUtilities()
+    : m_bootMode(0), m_booted(false), m_ovrSetFile(false), m_numModules(0) {}
 
 /**
  * @details
- * Reads in configuration information from cfgPixie16.txt, loads settings file 
+ * Reads in configuration information from cfgPixie16.txt, loads settings file
  * information, boots modules and saves configuration info.
  */
-int
-CPixieSystemUtilities::Boot()
-{
-    // If the settings file path has been overwritten pre system boot, use
-    // the new path. first we grab it, then reset it after initializing
-    // the configuration settings below.
-    
-    std::string newSetFile;
-    if (m_ovrSetFile) {
-	newSetFile = m_config.getSettingsFilePath();
-    }
+int CPixieSystemUtilities::Boot() {
+  // If the settings file path has been overwritten pre system boot, use
+  // the new path. first we grab it, then reset it after initializing
+  // the configuration settings below.
 
-    // If a FW file is specified, use it, otherwise use managed FW:
-    
-    const char* fwFile = getenv("FIRMWARE_FILE");
-    if (fwFile) {    
-	m_config = *(Configuration::generate(fwFile, "cfgPixie16.txt",
-					     "modevtlen.txt"));
-    } else { 
-	m_config = *(Configuration::generateManagedFW("cfgPixie16.txt",
-						      "modevtlen.txt"));
-    }
+  std::string newSetFile;
+  if (m_ovrSetFile) {
+    newSetFile = m_config.getSettingsFilePath();
+  }
 
-    // (Re)set the custom settings file path here if used:
-    
-    if (m_ovrSetFile) {
-	m_config.setSettingsFilePath(newSetFile);
-    }
+  // If a FW file is specified, use it, otherwise use managed FW:
 
-    /** 
-     * @note (ASC 9/11/24): Check the same envvar as e.g. the readout code to 
-     * determine whether to perform a full boot or settings-only boot. 
-     * In principle this could be configurable on the QtScope GUI but for now 
-     * the boot mode is set the same way as it is for the readout code. 
-     * An important thing to keep in mind is that 
-     * `getenv("DDAS_BOOT_WHEN_REQUESTED")` is false iff 
-     * `DDAS_BOOT_WHEN_REQUESTED` is not set (`getenv()` returns pointer to 
-     * the value string which evaluates to true regardless of the value 
-     * itself). When running a containerized NSCLDAQ one needs to make sure the
-     * envvar is set _inside_ the container.
-     */
-    
-    SystemBooter::BootType type = SystemBooter::FullBoot;
-    if (getenv("DDAS_BOOT_WHEN_REQUESTED")) type = SystemBooter::SettingsOnly;
-    SystemBooter booter;  
-    booter.setOfflineMode(m_bootMode); // 1: offline, 0: online
-    try {
-	booter.boot(m_config, type);
-    }
-    catch (const CXIAException& e) {
-	std::cerr << e.ReasonText() << std::endl;    
-	return -1;
-    }
+  const char *fwFile = getenv("FIRMWARE_FILE");
+  if (fwFile) {
+    m_config =
+        *(Configuration::generate(fwFile, "cfgPixie16.txt", "modevtlen.txt"));
+  } else {
+    m_config =
+        *(Configuration::generateManagedFW("cfgPixie16.txt", "modevtlen.txt"));
+  }
 
-    m_numModules = m_config.getNumberOfModules();
-    m_modEvtLength = m_config.getModuleEventLengths();
-    
-    // The hardware map is set up during boot time:
-    
-    std::vector<int> hdwrMap = m_config.getHardwareMap();
-    for (size_t i = 0; i < hdwrMap.size(); i++) {
-	HR::HardwareSpecification spec = HR::getSpecification(hdwrMap.at(i));
-	m_modADCMSPS.push_back(spec.s_adcFrequency);
-	m_modADCBits.push_back(spec.s_adcResolution);
-	m_modRev.push_back(spec.s_hdwrRevision);
-	m_modClockCal.push_back(spec.s_clockCalibration);
-    }
+  // (Re)set the custom settings file path here if used:
 
-    m_booted = true;
+  if (m_ovrSetFile) {
+    m_config.setSettingsFilePath(newSetFile);
+  }
 
-    return 0;
+  /**
+   * @note (ASC 9/11/24): Check the same envvar as e.g. the readout code to
+   * determine whether to perform a full boot or settings-only boot.
+   * In principle this could be configurable on the QtScope GUI but for now
+   * the boot mode is set the same way as it is for the readout code.
+   * An important thing to keep in mind is that
+   * `getenv("DDAS_BOOT_WHEN_REQUESTED")` is false iff
+   * `DDAS_BOOT_WHEN_REQUESTED` is not set (`getenv()` returns pointer to
+   * the value string which evaluates to true regardless of the value
+   * itself). When running a containerized NSCLDAQ one needs to make sure the
+   * envvar is set _inside_ the container.
+   */
+
+  SystemBooter::BootType type = SystemBooter::FullBoot;
+  if (getenv("DDAS_BOOT_WHEN_REQUESTED"))
+    type = SystemBooter::SettingsOnly;
+  SystemBooter booter;
+  booter.setOfflineMode(m_bootMode); // 1: offline, 0: online
+  try {
+    booter.boot(m_config, type);
+  } catch (const CXIAException &e) {
+    std::cerr << e.ReasonText() << std::endl;
+    return -1;
+  }
+
+  m_numModules = m_config.getNumberOfModules();
+  m_modEvtLength = m_config.getModuleEventLengths();
+
+  // The hardware map is set up during boot time:
+
+  std::vector<int> hdwrMap = m_config.getHardwareMap();
+  for (size_t i = 0; i < hdwrMap.size(); i++) {
+    HR::HardwareSpecification spec = HR::getSpecification(hdwrMap.at(i));
+    m_modADCMSPS.push_back(spec.s_adcFrequency);
+    m_modADCBits.push_back(spec.s_adcResolution);
+    m_modRev.push_back(spec.s_hdwrRevision);
+    m_modClockCal.push_back(spec.s_clockCalibration);
+  }
+
+  m_booted = true;
+
+  return 0;
 }
 
 /**
  * @details
- * File format depends on what is supported by the version of the XIA API 
- * being used. Version 3+ will save the settings file as a JSON file while in 
+ * File format depends on what is supported by the version of the XIA API
+ * being used. Version 3+ will save the settings file as a JSON file while in
  * version 2 it is binary.
  */
-int
-CPixieSystemUtilities::SaveSetFile(char* fileName)
-{
-    int retval;
-    try {
-	int retval = Pixie16SaveDSPParametersToFile(fileName);
-    
-	if (retval < 0) {
-	    std::stringstream msg;
-	    msg << "CPixieSystemUtilities::SaveSetFile() failed to save"
-		<< " DSP parameter file to: " << fileName;
-	    throw CXIAException(
-		msg.str(), "Pixie16SaveDSPParametersToFile()", retval
-		);
-	}
+int CPixieSystemUtilities::SaveSetFile(char *fileName) {
+  int retval;
+  try {
+    int retval = Pixie16SaveDSPParametersToFile(fileName);
+
+    if (retval < 0) {
+      std::stringstream msg;
+      msg << "CPixieSystemUtilities::SaveSetFile() failed to save"
+          << " DSP parameter file to: " << fileName;
+      throw CXIAException(msg.str(), "Pixie16SaveDSPParametersToFile()",
+                          retval);
     }
-    catch (const CXIAException& e) {
-	std::cerr << e.ReasonText() << std::endl;
-    }
-  
-    return retval;
+  } catch (const CXIAException &e) {
+    std::cerr << e.ReasonText() << std::endl;
+  }
+
+  return retval;
 }
 
 /**
  * @details
- * Check and see if the system is booted. If so, load the parameters from 
- * the settings file. If not flag that a new settings file path (potentially 
- * different from that in the cfgPixie16.txt) has been set. The flag is 
+ * Check and see if the system is booted. If so, load the parameters from
+ * the settings file. If not flag that a new settings file path (potentially
+ * different from that in the cfgPixie16.txt) has been set. The flag is
  * checked at boot to load the new settings file.
  */
-int
-CPixieSystemUtilities::LoadSetFile(char* fileName)
-{
-    int retval;
-    try {  
-	if(m_booted) { // If system is booted just apply the params.    
-	    retval = Pixie16LoadDSPParametersFromFile(fileName);
-    
-	    if (retval < 0) {
-		std::stringstream msg;
-		msg << "CPixieSystemUtilities::LoadSetFile() failed to"
-		    << " load DSP parameter file from: " << fileName;
-		throw CXIAException(
-		    msg.str(), "Pixie16LoadDSPParametersFromFile()", retval
-		    );
-	    } else {
-		std::cout << "Loading new DSP parameter file from: "
-			  << fileName << std::endl;
-	    }  
-	} else { // Not booted so hold on to the name.
-	    m_ovrSetFile = true;
-	    m_config.setSettingsFilePath(fileName);
-	    std::cout << "New DSP parameter file " << fileName
-		      << " will be loaded on system boot" << std::endl;
-	}
+int CPixieSystemUtilities::LoadSetFile(char *fileName) {
+  int retval;
+  try {
+    if (m_booted) { // If system is booted just apply the params.
+      retval = Pixie16LoadDSPParametersFromFile(fileName);
+
+      if (retval < 0) {
+        std::stringstream msg;
+        msg << "CPixieSystemUtilities::LoadSetFile() failed to"
+            << " load DSP parameter file from: " << fileName;
+        throw CXIAException(msg.str(), "Pixie16LoadDSPParametersFromFile()",
+                            retval);
+      } else {
+        std::cout << "Loading new DSP parameter file from: " << fileName
+                  << std::endl;
+      }
+    } else { // Not booted so hold on to the name.
+      m_ovrSetFile = true;
+      m_config.setSettingsFilePath(fileName);
+      std::cout << "New DSP parameter file " << fileName
+                << " will be loaded on system boot" << std::endl;
     }
-    catch (const CXIAException& e) {
-	std::cerr << e.ReasonText() << std::endl;
-	return e.ReasonCode();
-    }
-  
-    return retval;
+  } catch (const CXIAException &e) {
+    std::cerr << e.ReasonText() << std::endl;
+    return e.ReasonCode();
+  }
+
+  return retval;
 }
 
 /**
  * @details
- * If the call to Pixie16ExitSystem() fails for any module, return the error 
- * code and set the booted state flag to false. The system is likely in a bad 
+ * If the call to Pixie16ExitSystem() fails for any module, return the error
+ * code and set the booted state flag to false. The system is likely in a bad
  * state.
  */
-int
-CPixieSystemUtilities::ExitSystem()
-{
-    int retval;
-    try {
-	if (m_booted) {
-	    for (int i = 0 ; i < m_numModules; i++) {      
-		retval = Pixie16ExitSystem(i);      
-		if (retval < 0) {
-		    std::stringstream msg;
-		    msg << "CPixieSystemUtilities::ExitSystem() failed"
-			<< " to exit " << "module " << i;
-		    throw CXIAException(
-			msg.str(), "Pixie16ExitSystem()", retval
-			);
-		}
-	    }    
-	}  
-	m_booted = false;
+int CPixieSystemUtilities::ExitSystem() {
+  int retval;
+  try {
+    if (m_booted) {
+      for (int i = 0; i < m_numModules; i++) {
+        retval = Pixie16ExitSystem(i);
+        if (retval < 0) {
+          std::stringstream msg;
+          msg << "CPixieSystemUtilities::ExitSystem() failed"
+              << " to exit " << "module " << i;
+          throw CXIAException(msg.str(), "Pixie16ExitSystem()", retval);
+        }
+      }
     }
-    catch (const CXIAException& e) {
-	std::cerr << e.ReasonText();
-	m_booted = false;
-	return e.ReasonCode();
-    }
-      
-    return retval; // All good.
+    m_booted = false;
+  } catch (const CXIAException &e) {
+    std::cerr << e.ReasonText();
+    m_booted = false;
+    return e.ReasonCode();
+  }
+
+  return retval; // All good.
 }
 
-int
-CPixieSystemUtilities::GetModuleMSPS(int module)
-{
-    // A correctly booted system must by definition contain >= 1 module
-    // so I'm _pretty_ sure this is a good check for that too:
-    
-    if (!m_booted) {
-	std::string msg(
-	    "CPixieSystemUtilities::GetModuleMSPS() system not booted."
-	    );
-	return -1;
-    } else if ((module < 0) || (module >= m_numModules)) {
-	std::stringstream msg;
-	msg << "CPixieSystemUtilities::GetModuleMSPS() ";
-	msg << "invalid module number ";
-	msg << module << " for " << m_numModules << " module system.";
-	std::cerr << msg.str() << std::endl;
-	return -2;
-    } else {
-	// Implicit conversion probably OK but:
-	return static_cast<int>(m_modADCMSPS[module]);
-    }
+int CPixieSystemUtilities::GetModuleMSPS(int module) {
+  // A correctly booted system must by definition contain >= 1 module
+  // so I'm _pretty_ sure this is a good check for that too:
+
+  if (!m_booted) {
+    std::string msg(
+        "CPixieSystemUtilities::GetModuleMSPS() system not booted.");
+    return -1;
+  } else if ((module < 0) || (module >= m_numModules)) {
+    std::stringstream msg;
+    msg << "CPixieSystemUtilities::GetModuleMSPS() ";
+    msg << "invalid module number ";
+    msg << module << " for " << m_numModules << " module system.";
+    std::cerr << msg.str() << std::endl;
+    return -2;
+  } else {
+    // Implicit conversion probably OK but:
+    return static_cast<int>(m_modADCMSPS[module]);
+  }
 }
 
-int
-CPixieSystemUtilities::GetChannelCount(int module)
-{
-    // A correctly booted system must by efinition contain >= 1 module
-    // so I'm _pretty_ sure this is a good check for that too:
-    
-    if (!m_booted) {
-	std::string msg("CPixieSystemUtilities::GetChannelCount() system not booted.");
-	std::cerr << msg << std::endl;
-	return -1;
-    } else if ((module < 0) || (module >= m_numModules)) {
-	std::stringstream msg;
-	msg << "CPixieSystemUtilities::GetChannelCount() ";
-	msg << "invalid module number ";
-	msg << module << " for " << m_numModules << " module system.";
-	std::cerr << msg.str() << std::endl;
-	return -2;
-    } else {
-	// Implicit conversion probably OK but:
-	return static_cast<int>(m_config.getChannelCount(module));
-    }
+int CPixieSystemUtilities::GetChannelCount(int module) {
+  // A correctly booted system must by efinition contain >= 1 module
+  // so I'm _pretty_ sure this is a good check for that too:
+
+  if (!m_booted) {
+    std::string msg(
+        "CPixieSystemUtilities::GetChannelCount() system not booted.");
+    std::cerr << msg << std::endl;
+    return -1;
+  } else if ((module < 0) || (module >= m_numModules)) {
+    std::stringstream msg;
+    msg << "CPixieSystemUtilities::GetChannelCount() ";
+    msg << "invalid module number ";
+    msg << module << " for " << m_numModules << " module system.";
+    std::cerr << msg.str() << std::endl;
+    return -2;
+  } else {
+    // Implicit conversion probably OK but:
+    return static_cast<int>(m_config.getChannelCount(module));
+  }
 }
--- main/ddas/qtscope/CPixieTraceUtilities.h
+++ main/ddas/qtscope/CPixieTraceUtilities.h
@@ -1,6 +1,6 @@
 /**
  * @file  CPixieTraceUtilities.h
- * @brief Defines a class for trace management and a ctypes interface for 
+ * @brief Defines a class for trace management and a ctypes interface for
  * the class.
  */
 
@@ -13,7 +13,7 @@ class CDataGenerator;
 
 /**
  * @addtogroup utilities libPixieUtilities.so
- * @{ 
+ * @{
  */
 
 /**
@@ -21,131 +21,126 @@ class CDataGenerator;
  * @brief A class to read and fetch trace data from Pixie-16 modules.
  *
  * @details
- * This class provides a ctypes-friendly interface to acquire "validated" 
- * (traces which are likely to contain a good signal pulse) and unvalidated 
+ * This class provides a ctypes-friendly interface to acquire "validated"
+ * (traces which are likely to contain a good signal pulse) and unvalidated
  * traces. The class also provides methods to access the trace data.
  *
- * @todo Instead of validated traces can we process the trace using the fast 
+ * @todo Instead of validated traces can we process the trace using the fast
  * filter parameters and wait for a real trigger?
  */
 
-class CPixieTraceUtilities
-{
+class CPixieTraceUtilities {
 private:
-    CDataGenerator* m_pGenerator; //!< The offline data generator.
-    bool m_useGenerator; //!< True if using generated data, else online data.
-    std::vector<unsigned short> m_trace; //!< Single channel trace data.
-    double m_validAmplitude; //!< Minimum amplitude for a validated trace.
-    
+  CDataGenerator *m_pGenerator; //!< The offline data generator.
+  bool m_useGenerator; //!< True if using generated data, else online data.
+  std::vector<unsigned short> m_trace; //!< Single channel trace data.
+  double m_validAmplitude; //!< Minimum amplitude for a validated trace.
+
 public:
-    /** @brief Constructor. */
-    CPixieTraceUtilities();
-    /** @brief Destructor. */
-    ~CPixieTraceUtilities();
-    
-    /**
-     * @brief Read a validated ADC trace from single channel.
-     * @param module  Module number.
-     * @param channel Channel number on module for trace read.
-     * @return int
-     * @retval  0 Success.
-     * @retval -1 XIA API call fails.
-     * @retval -2 Acquired trace is empty (median undefined).
-     * @retval -3 Unhanded exception when reading trace and calculating median.
-     */
-    int ReadTrace(int module, int channel);
-    /**
-     * @brief Read a validated ADC trace from single channel.
-     * @param module  Module number.
-     * @param channel Channel number on module for trace read.
-     * @return int
-     * @retval  0 Success.
-     * @retval -1 XIA API call fails.
-     */
-    int ReadFastTrace(int module, int channel);
-    /**
-     * @brief Return the trace data.
-     * @return Pointer to the underlying trace storage.
-     */
-    unsigned short* GetTraceData() { return m_trace.data(); }
-    /**
-     * @brief Set the flag for offline mode using the data generator.
-     * @param mode The generator flag is set to this input value.
-     */
-    void SetUseGenerator(bool mode) { m_useGenerator = mode; }
-    /**
-     * @brief Get the trace length for a module.
-     * @param module Module number.
-     * @return Trace length in samples or XIA error code if failed.
-     */
-    int GetTraceLength(int module);
-  
+  /** @brief Constructor. */
+  CPixieTraceUtilities();
+  /** @brief Destructor. */
+  ~CPixieTraceUtilities();
+
+  /**
+   * @brief Read a validated ADC trace from single channel.
+   * @param module  Module number.
+   * @param channel Channel number on module for trace read.
+   * @return int
+   * @retval  0 Success.
+   * @retval -1 XIA API call fails.
+   * @retval -2 Acquired trace is empty (median undefined).
+   * @retval -3 Unhanded exception when reading trace and calculating median.
+   */
+  int ReadTrace(int module, int channel);
+  /**
+   * @brief Read a validated ADC trace from single channel.
+   * @param module  Module number.
+   * @param channel Channel number on module for trace read.
+   * @return int
+   * @retval  0 Success.
+   * @retval -1 XIA API call fails.
+   */
+  int ReadFastTrace(int module, int channel);
+  /**
+   * @brief Return the trace data.
+   * @return Pointer to the underlying trace storage.
+   */
+  unsigned short *GetTraceData() { return m_trace.data(); }
+  /**
+   * @brief Set the flag for offline mode using the data generator.
+   * @param mode The generator flag is set to this input value.
+   */
+  void SetUseGenerator(bool mode) { m_useGenerator = mode; }
+  /**
+   * @brief Get the trace length for a module.
+   * @param module Module number.
+   * @return Trace length in samples or XIA error code if failed.
+   */
+  int GetTraceLength(int module);
+
 private:
-    /**
-     * @brief Call to Pixie-16 API to acquire an ADC trace from a single 
-     * channel.
-     * @param module  Module number.
-     * @param channel Channel number on module for trace read.
-     * @throw std::runtime_error If ADC traces cannot be acquired (internal
-     *   DSP memory fails to fill).
-     * @throws CXIAException If trace allocation or read fails.
-     */
-    void AcquireADCTrace(int module, int channel);
-    /**
-     * @brief Calculate the median value from a trace.
-     * @param v Input vector of type T.
-     * @throw std::invalid_argument If trace is empty (median is undefined).
-     * @return Median value of the trace.
-     */
-    template<typename T> double GetMedianValue(std::vector<T> v);
-    /**
-     * @brief Reset the trace storage vector.
-     * @param len Trace length to read.
-     */
-    void ResetTrace(unsigned int len);
+  /**
+   * @brief Call to Pixie-16 API to acquire an ADC trace from a single
+   * channel.
+   * @param module  Module number.
+   * @param channel Channel number on module for trace read.
+   * @throw std::runtime_error If ADC traces cannot be acquired (internal
+   *   DSP memory fails to fill).
+   * @throws CXIAException If trace allocation or read fails.
+   */
+  void AcquireADCTrace(int module, int channel);
+  /**
+   * @brief Calculate the median value from a trace.
+   * @param v Input vector of type T.
+   * @throw std::invalid_argument If trace is empty (median is undefined).
+   * @return Median value of the trace.
+   */
+  template <typename T> double GetMedianValue(std::vector<T> v);
+  /**
+   * @brief Reset the trace storage vector.
+   * @param len Trace length to read.
+   */
+  void ResetTrace(unsigned int len);
 };
 
 /** @} */
 
 extern "C" {
-    /** @brief Wrapper for the class constructor. */
-    CPixieTraceUtilities* CPixieTraceUtilities_new()
-    {
-	return new CPixieTraceUtilities();
-    }
-    /** @brief Wrapper for reading a validated trace. */
-    int CPixieTraceUtilities_ReadTrace(CPixieTraceUtilities* utils, int mod, int chan)
-    {
-	return utils->ReadTrace(mod, chan);
-    }
-    /** @brief Wrapper for reading an unvalidated trace. */
-    int CPixieTraceUtilities_ReadFastTrace(CPixieTraceUtilities* utils, int mod, int chan)
-    {
-	return utils->ReadFastTrace(mod, chan);
-    }
-    /** @brief Wrapper to get trace length. */
-    int CPixieTraceUtilities_GetTraceLength(CPixieTraceUtilities* utils, int mod)
-    {
-    return utils->GetTraceLength(mod);
-    }
-    /** @brief Wrapper to get trace data. */
-    unsigned short* CPixieTraceUtilities_GetTraceData(CPixieTraceUtilities* utils)
-    {
-	return utils->GetTraceData();
-    }
-    /** @brief Wrapper to set generator use. */
-    void CPixieTraceUtilities_SetUseGenerator(CPixieTraceUtilities* utils, bool mode)
-    {
-	return utils->SetUseGenerator(mode);
-    }
-    /** @brief Wrapper for the class destructor. */
-    void CPixieTraceUtilities_delete(CPixieTraceUtilities* utils)
-    {
-	if(utils) {
-	    delete utils;
-	    utils = nullptr;
-	}
-    }
+/** @brief Wrapper for the class constructor. */
+CPixieTraceUtilities *CPixieTraceUtilities_new() {
+  return new CPixieTraceUtilities();
+}
+/** @brief Wrapper for reading a validated trace. */
+int CPixieTraceUtilities_ReadTrace(CPixieTraceUtilities *utils, int mod,
+                                   int chan) {
+  return utils->ReadTrace(mod, chan);
+}
+/** @brief Wrapper for reading an unvalidated trace. */
+int CPixieTraceUtilities_ReadFastTrace(CPixieTraceUtilities *utils, int mod,
+                                       int chan) {
+  return utils->ReadFastTrace(mod, chan);
+}
+/** @brief Wrapper to get trace length. */
+int CPixieTraceUtilities_GetTraceLength(CPixieTraceUtilities *utils, int mod) {
+  return utils->GetTraceLength(mod);
+}
+/** @brief Wrapper to get trace data. */
+unsigned short *CPixieTraceUtilities_GetTraceData(CPixieTraceUtilities *utils) {
+  return utils->GetTraceData();
+}
+/** @brief Wrapper to set generator use. */
+void CPixieTraceUtilities_SetUseGenerator(CPixieTraceUtilities *utils,
+                                          bool mode) {
+  return utils->SetUseGenerator(mode);
+}
+/** @brief Wrapper for the class destructor. */
+void CPixieTraceUtilities_delete(CPixieTraceUtilities *utils) {
+  if (utils) {
+    delete utils;
+    utils = nullptr;
+  }
+}
 }
 
 #endif
