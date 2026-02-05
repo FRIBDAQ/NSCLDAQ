cbaad34f18d6b3110381aec909bc7fabc0554b40
Chester, Aaron <chestera@msu.edu>
2026-02-05 15:04:55 -0500

    even more formatting

--- main/ddas/qtscope/qtscope.cpp
+++ main/ddas/qtscope/qtscope.cpp
@@ -24,28 +24,25 @@
  * @details
  * Uses Python C++ API to call main.py to configure and run QtScope.
  */
-int main(int argc, char *argv[])
-{  
-    Py_Initialize();
-  
-    try {
-	std::string filename(PREFIX"/ddas/qtscope/main.py");
-	PyObject *obj = Py_BuildValue("s", filename.c_str());    
-	FILE *file = _Py_fopen_obj(obj, "r");
-	if(file != NULL) {
-	    PyRun_SimpleFile(file, filename.c_str());
-	} else {
-	    std::string errmsg = "Cannot open QtScope main from: " + filename;
-	    throw std::invalid_argument(errmsg);
-	}
-    }
-    catch (std::exception& e) {
-	std::cout << "QtScope main caught an exception: "
-		  << e.what() << std::endl;
-	exit(EXIT_FAILURE);
-    }
+int main(int argc, char *argv[]) {
+  Py_Initialize();
 
-    Py_Finalize();
+  try {
+    std::string filename(PREFIX "/ddas/qtscope/main.py");
+    PyObject *obj = Py_BuildValue("s", filename.c_str());
+    FILE *file = _Py_fopen_obj(obj, "r");
+    if (file != NULL) {
+      PyRun_SimpleFile(file, filename.c_str());
+    } else {
+      std::string errmsg = "Cannot open QtScope main from: " + filename;
+      throw std::invalid_argument(errmsg);
+    }
+  } catch (std::exception &e) {
+    std::cout << "QtScope main caught an exception: " << e.what() << std::endl;
+    exit(EXIT_FAILURE);
+  }
 
-    return 0;
+  Py_Finalize();
+
+  return 0;
 }
