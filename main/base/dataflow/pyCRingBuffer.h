345a82435cfb0d987039d124b34016e9e5471eac
Ron Fox <fox@frib.msu.edu>
2026-05-05 09:13:16 -0400

    Missing from repository :-(

---
+++ main/base/dataflow/pyCRingBuffer.h
@@ -0,0 +1,33 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2014-2025.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+	     FRIB
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+/**
+ * @file pyCRingBuffer.h
+ * @brief Header for a python callable scaled down ringbuffer class
+ * @author Ron Fox
+ */
+#ifndef PYCRINGBUFFER_H
+#define PYCRINGBUFFER_H
+
+
+#ifndef PY_SSIZE_T_CLEAN
+#define PY_SSIZE_T_CLEAN
+#endif
+
+#include <Python.h>
+
+
+
+#endif
+
+
