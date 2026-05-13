8ae1deb94b7c3c33bf6378e24feb42af5d8cd405
Ron Fox <fox@frib.msu.edu>
2026-05-13 07:53:02 -0400

    Issue #489 - Python scaler display program
    Added bare bones model that sets the headers and
    added it to the test code so we get a table that's not
    totally empty

--- main/utilities/pyscaler/ScalerPage.py
+++ main/utilities/pyscaler/ScalerPage.py
@@ -23,7 +23,51 @@
 from PyQt5.QtWidgets import QLabel, QTableView, QVBoxLayout, QHBoxLayout, QWidget
 from PyQt5.QtGui     import QStandardItemModel, QStandardItem
 
+##------------------------ The model class: ----------------------------
 
+class ScalerPageModel(QStandardItemModel):
+    '''
+        This model should be attached to the SclaerPageView and 
+        its contents appropriately updated with time.
+        
+        The model provides a bit more scaler like interface.
+        lines in the model have the following columns:
+        
+        scaler1  counts1 rate1 scaler 2 counts1 rate1  counts-ratio rate-ratio
+        
+        The model supports three kinds of rows: 
+        
+        * single - only scaler1, counts1 rate1 are populated.
+        * pair   - saler1, counts1, rate1 scaler2 counts2 rate2 are populated
+        * ratio  - All items are populated.
+        
+        Local data maintains the type of each row, which can be queried
+        along with the scaler names, and the count values and so on.
+        
+        A row can be updated by passing a dict containing the keys:
+        
+        type - the type of row - checked against the model row.
+        scalers - array of scaler names
+        counts  - array of counts
+        rates   - array of rates.
+        
+        Note that the size of the counter arrays are not range checked.
+        It's up tot the caller to ensure they are correct for the type.
+        
+        A vertical header is created with column labels.
+    '''
+    
+    def __init__(self, *args):
+        super().__init__(*args)
+        
+        # All we do here is set the horizontal headers:
+        
+        self.setHorizontalHeaderLabels([
+            'Scaler', 'counts', 'rate',
+            'Scaler', 'counts', 'rate',
+            'ratio(counts)', 'ratio(rates)'
+        ])
+        
 ##------------------------ The view class:   ----------------------------
 
 
@@ -82,6 +126,10 @@ if __name__ == '__main__':
     widget = ScalerPageView(win)
     win.setCentralWidget(widget)
     widget.setTitle('Some random page title')
+    
+    model = ScalerPageModel()
+    widget.setModel(model)
+    
     win.show()
     app.exec()
         
