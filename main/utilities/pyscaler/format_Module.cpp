b7ce3d6679fcd6d8ade2e79c434835185669448b
Ron Fox <fox@frib.msu.edu>
2026-05-14 10:01:32 -0400

    Issue #489 - Python scaler display program
    * Make a pretty empty format python module.
    * Add a Makefile that knows how to build/install the format
    python module.

---
+++ main/utilities/pyscaler/format_Module.cpp
@@ -0,0 +1,56 @@
+/*
+    This software is Copyright by the Board of Trustees of Michigan
+    State University (c) Copyright 2014-2025.
+
+    You may use this software under the terms of the GNU public license
+    (GPL).  The terms of this license are described at:
+
+     http://www.gnu.org/licenses/gpl.txt
+
+	     FRIB
+	     Michigan State University
+	     East Lansing, MI 48824-1321
+*/
+
+static const char* Copyright = "Copyright Michigan State University 2026, All rights reserved";
+
+/**
+ * @file format_Module.cpp
+ * @brief The Module code for the the DAQ format module.
+ * @author Ron Fox
+ */
+#define PY_SSIZE_T_CLEAN
+#include <Python.h>
+#include <NSCLDAQFormatFactorySelector.h>
+#include <RingItemFactoryBase.h>
+#include <CRingItem.h>
+#include <CDataFormatItem.h>
+#include <CRingStateChangeItem.h>
+#include <CRingScalerItem.h>
+
+
+// The module level methods. 
+// These have to do with making the apropriate factories:
+
+static PyMethodDef format_methods[] = {
+    {nullptr, nullptr, 0, nullptr}                // End of table sentinel.
+};
+
+// Module definition table:
+
+static PyModuleDef format_module = {
+    .m_base = PyModuleDef_HEAD_INIT,
+    .m_name = "format",
+    .m_doc  = "Provides version independent ring item format handling",
+    .m_size = 0,
+    .m_methods = format_methods,
+
+};
+
+extern "C" {
+PyMODINIT_FUNC
+PyInit_format(void) {
+    return PyModuleDef_Init(&format_module);
+}
+
+}
