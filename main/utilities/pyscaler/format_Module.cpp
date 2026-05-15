5e85e48b76121c078cd376d62a2899731c877ffc
Ron Fox <fox@frib.msu.edu>
2026-05-15 08:23:37 -0400

    Issue #489 - Python scaler display program
    Added a ringitemfactory type:
    *  Construction constructs a factory of the selected version.
    *  makeRingItem method -- in progress uses the factory to  make
    a ringitem type I need to add

--- main/utilities/pyscaler/format_Module.cpp
+++ main/utilities/pyscaler/format_Module.cpp
@@ -21,37 +21,15 @@ static const char* Copyright = "Copyright Michigan State University 2026, All ri
  */
 #define PY_SSIZE_T_CLEAN
 #include <Python.h>
-#include <NSCLDAQFormatFactorySelector.h>
-#include <RingItemFactoryBase.h>
-#include <CRingItem.h>
-#include <CDataFormatItem.h>
-#include <CRingStateChangeItem.h>
-#include <CRingScalerItem.h>
 
+#include "format_factory.h"
 
-#include "format_selector.h"
-
-/**
- * Given a format selector number, returns the 
- * appropriate format factory object.  The format
- * factory type object is defined in the format_selector.h header.
- * 
- */
-static PyObject*
-select_factory(PyObject* self, PyObject* args) {
-    int format;
-    if(!PyArg_ParseTuple(args, "i", &format)) {
-        return nullptr;                               // AN exception was raised.
-    }
-    return nullptr;
-}
 
 
 // The module level methods. 
 // These have to do with making the apropriate factories:
 
 static PyMethodDef format_methods[] = {
-    {"selectFactory", select_factory, METH_VARARGS, "Select a ring item factory from the NSCLDAQ version number" },
     {nullptr, nullptr, 0, nullptr}                // End of table sentinel.
 };
 
@@ -59,7 +37,7 @@ static PyMethodDef format_methods[] = {
 
 static PyModuleDef format_module = {
     .m_base = PyModuleDef_HEAD_INIT,
-    .m_name = "format",
+    .m_name = "daqformat",
     .m_doc  = "Provides version independent ring item format handling",
     .m_size = 0,
     .m_methods = format_methods,
@@ -68,13 +46,13 @@ static PyModuleDef format_module = {
 
 extern "C" {
 PyMODINIT_FUNC
-PyInit_format(void) {
+PyInit_daqformat(void) {
     PyModuleDef_Init(&format_module);
     auto module = PyModule_Create(&format_module);
     if (PyType_Ready(&pyRingItemFactoryType) < 0) {
         return NULL;
     }
-    if (PyModule_AddObjectRef(module, "format", (PyObject*)(&pyRingItemFactoryType)) < 0) {
+    if (PyModule_AddObjectRef(module, "ringitemfactory", (PyObject*)(&pyRingItemFactoryType)) < 0) {
         return NULL;
     }
     return module;
