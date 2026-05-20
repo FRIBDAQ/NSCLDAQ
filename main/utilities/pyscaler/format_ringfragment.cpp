ba7cb1c4d3859ea1dc17cc8c3463e304754e704f
Ron Fox <fox@frib.msu.edu>
2026-05-20 08:25:53 -0400

    Issue #489 - Python scaler display program
    Thought a bit better of the name of the ring fragment class

--- main/utilities/pyscaler/format_ringfragment.cpp
+++ main/utilities/pyscaler/format_ringfragment.cpp
@@ -58,7 +58,7 @@ static struct PyMethodDef methods[] = {
 
 PyTypeObject pyRingFragmentType = {
     .ob_base = PyVarObject_HEAD_INIT(NULL, 0)
-    .tp_name = "ringfragmentobject",
+    .tp_name = "ringfragmentitem",
     .tp_basicsize = sizeof(pyRingFragmentItem),
     .tp_itemsize = 0,
     .tp_flags = Py_TPFLAGS_DEFAULT,
