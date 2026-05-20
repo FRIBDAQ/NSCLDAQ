b58580eda9cb61a3daaf281cff040a6786d13c0a
Ron Fox <fox@frib.msu.edu>
2026-05-20 09:03:16 -0400

    Issue #489 - Python scaler display program
    Implemented all of the methods for the encapsulated
    ring fragment item

--- main/utilities/pyscaler/format_ringfragment.cpp
+++ main/utilities/pyscaler/format_ringfragment.cpp
@@ -48,9 +48,70 @@ init_basetype(PyObject* self, PyObject* args, PyObject* kwargs) {
 
 }
 
+/////// Method implementations:
+
+/**
+ * timestamp
+ *    Return the timestamp of the fragment.
+ * @param self - pointer to the object that called us.
+ * @param args - position parameters, not used.
+ * @return PyObject* object containing the timestamp.
+ */
+static PyObject*
+timestamp(PyObject* self, PyObject* args) {
+    pyRingFragmentItem* pThis = reinterpret_cast<pyRingFragmentItem*>(self);
+    return PyLong_FromUnsignedLongLong(pThis->m_pItem->timestamp());
+}
+/**
+ * source
+ *    Return the source id of the source that contributed the fragment.
+ * @param self -pointer to the object that called us.
+ * @param args - unused position arguments.
+ * @return PyObject* object containing the source id.
+ * 
+ */
+static PyObject*
+source(PyObject* self, PyObject* args) {
+    pyRingFragmentItem* pThis = reinterpret_cast<pyRingFragmentItem*>(self);
+    return PyLong_FromUnsignedLong(pThis->m_pItem->source());
+}
+/**
+ * payload
+ *    Returns the fragment payload as a bytearra.
+ * 
+ * @param self - pointer to the object calling us.
+ * @param args - unused positional args.
+ * @return PyObject* - pointer to the byte array we produce.
+ */
+static PyObject*
+payload(PyObject* self, PyObject* args) {
+    pyRingFragmentItem* pThis = reinterpret_cast<pyRingFragmentItem*>(self);
+    ufmt::CRingFragmentItem* pItem = pThis->m_pItem;    // For notational simplicty.
+    size_t nBytes = pItem->payloadSize();
+    return PyBytes_FromStringAndSize(
+        reinterpret_cast<char*>(pItem->payloadPointer()), nBytes
+    );
+}
+/**
+ * barrierType
+ *  return the fragment barrier type.
+ * @param self -pointer to the object calling us.
+ * @param args - unused positional arg.
+ * @return PyObject* long containing the barrier.
+ * 
+ */
+static PyObject*
+barrierType(PyObject* self, PyObject* args) {
+    pyRingFragmentItem* pThis = reinterpret_cast<pyRingFragmentItem*>(self);
+    return PyLong_FromUnsignedLong(pThis->m_pItem->barrierType());
+}
 // THe method table:
 
 static struct PyMethodDef methods[] = {
+    {"timestamp", timestamp, METH_NOARGS, "Get the fragment timestamp"},
+    {"source", source, METH_NOARGS, "Get the fragment source id"}, 
+    {"payload", payload, METH_NOARGS, "Get the fragment payload as a byte array."},
+    {"barrierType", barrierType, METH_NOARGS, "Get the barrier type of the fragment"},
     {nullptr, nullptr, 0, nullptr}        // End of table sentinel.
 };
 
