752688e8728967213461c0855d4880ace059488f
Ron Fox <fox@frib.msu.edu>
2026-06-24 10:26:33 -0400

    Issue #496  Replace or augment Tcl code with Python equivalents
    * Coded model and view for ReadoutStatistics (triggers,accepted
      trigger/bytes).
    * Install via Makefile.am

---
+++ main/utilities/readoutREST/ReadoutStatistics.py
@@ -0,0 +1,144 @@
+'''
+    Provides the model and view for Readout statistics.  The readout provide the
+    per run and cumulative trigger statistics for a readout program.
+'''
+
+
+#    This software is Copyright by the Board of Trustees of Michigan
+#    State University (c) Copyright 2014, 2026
+#
+#    You may use this software under the terms of the GNU public license
+#    (GPL).  The terms of this license are described at:
+#
+#     http://www.gnu.org/licenses/gpl.txt
+#
+#	     FRIB
+#	     Michigan State University
+#	     East Lansing, MI 48824-1321
+
+
+from PyQt6.QtCore import QObject, Qt
+from PyQt6.QtGui  import QStandardItemModel, QStandardItem
+from PyQt6.QtWidgets import QTableView
+
+
+class RunStatisticsModel(QStandardItemModel):
+    '''
+        Provides a model containing the statistics. Since we inter-operate
+        with an e.g. QTableView, we don't need any signals as the model
+        will generate the right ones to up date the view.
+        
+        We're going to maintain the QStandardItem models in an
+        internal hash that has 'perRun' and 'cumulative' keys
+        which have as members the 'triggers', 'acceptedTriggers' and 'bytes' keys.
+    
+        We're also going to provide both vertical headers ("Per Run" and "Cumulative")
+        and horizontal heaers ('Triggers', 'Accepted Triggers' and 'Bytes of Data').
+        
+    Attributes:
+        statistics  - read/write where the representation is like what getStatistics in the
+        readouRestClient.ReadoutClient.getStatistics method returns.
+    '''
+    def __init__(self, parent : QObject = None):
+        super().__init__(parent)
+        
+        # Horizontal headers:
+         
+        self.setHorizontalHeaderLabels([' Triggers', ' Accepted Triggers', ' Bytes of Data'])
+        
+        # Now make the standard items and save them.. we initialize them to 0's.
+        
+        self._items = dict()                  # Where we'll store them for updating:
+        self._items['perRun'] = dict()
+        self._items['perRun']['triggers']         = self._makeItem()
+        self._items['perRun']['acceptedTriggers'] = self._makeItem()
+        self._items['perRun']['bytes']            = self._makeItem()
+        
+        self._items['cumulative'] = dict()
+        self._items['cumulative']['triggers']         = self._makeItem()
+        self._items['cumulative']['acceptedTriggers'] = self._makeItem()
+        self._items['cumulative']['bytes']            = self._makeItem()
+        
+        
+        # Put the items in the model:
+        
+        self.appendRow([
+            self._items['perRun']['triggers'],
+            self._items['perRun']['acceptedTriggers'], 
+            self._items['perRun']['bytes']]
+        )
+        self.appendRow([
+            self._items['cumulative']['triggers'],
+            self._items['cumulative']['acceptedTriggers'], 
+            self._items['cumulative']['bytes']]
+        )
+        # Vertical headers.
+        
+        self.setVerticalHeaderLabels(['Per Run', 'Cumulative'])
+        
+        
+        
+        
+    # implement properties:
+    
+    def statistics(self) -> dict[dict]:
+        '''
+          @return dict - a dictionary of the current statistics. See class docstring for
+             the keys this dict will have.
+        '''
+        result = dict()
+        for which,stats in self._items.items():
+            result[which] = dict()
+            for name,item in stats.items():
+                result[stats][name] = int(item.text())
+    
+        return result
+    
+    def setStatistics(self, statistics : dict[dict]) -> None:
+        '''
+        @param statistics - the new statistics.  This is a dict of dicts as desribed
+            in the class docstring.
+        '''
+            
+        for which, statistic in statistics:
+            for name, value in statistics:
+                self._items[which][name].setText(str(value))
+
+    #  Utilities:
+    
+    def _makeItem(self) -> QStandardItem:
+        # Utility to create a standard item with the flags we want.
+        item = QStandardItem('0')
+        item.setFlags(Qt.ItemFlag.ItemIsEnabled)   # Mostly we want to disable editing.
+        return item
+        
+        
+ # View
+    
+class RunStatistics(QTableView):
+    '''
+        We just need to hook ourselves into a model.
+    '''
+    def __init__(self, parent : QObject | None=None):
+        super().__init__(parent)
+        
+        self._model = RunStatisticsModel(self)
+        self.setModel(self._model)
+        
+        self.horizontalHeader().setStretchLastSection(True)
+        
+    
+## Test code:
+
+if __name__  == "__main__":
+    from PyQt6.QtWidgets import QApplication, QMainWindow
+    import sys
+
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    widget = RunStatistics(win)
+    
+    
+    win.setCentralWidget(widget)
+    win.show()
+    sys.exit(app.exec())
