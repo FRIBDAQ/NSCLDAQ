416d3058a45ea24cc2c831261fe51fb3f54d30b6
Ron Fox <fox@frib.msu.edu>
2026-07-01 07:31:22 -0400

    Issue #496  Replace or augment Tcl code with Python equivalents
    Add a timer to keep the model updated from the manager witness
    data

--- main/utilities/readoutREST/RunInfoController.py
+++ main/utilities/readoutREST/RunInfoController.py
@@ -21,21 +21,23 @@
 #	     Michigan State University
 #	     East Lansing, MI 48824-1321
 
+from PyQt6.QtCore import QTimer, QObject
+from nscldaq.readoutREST import rdo_utils
 
-
-class RunInfoController:
+class RunInfoController(QObject):
     '''
         This class is an intermediary between the RunInfo model/view
         and some underlying system that impelements the KVStore
         run and title convenience functions.
         
     '''
-    def __init__(self, view, client):
+    def __init__(self, view, client, parent=None):
         '''
             @param view - a RunInfo compatible object.
             @param client - A KVStore compiatible object. 
+            @param parent - our parent object if there is one.
         '''
-        
+        super().__init__(parent)
         self._view = view
         self._model = view.model()
         self._client = client
@@ -54,12 +56,29 @@ class RunInfoController:
         self._view.setRun(run)
         
         
-        #  Set up signal handlesr to keep our client updated:
+        #  Set up signal handlers to keep our client updated:
         
         self._view.requestedTitleChanged.connect(self._updateTitle)
         self._view.requestedRunChanged.connect(self._updateRun)
         
+        # There might be other controllers so we need to
+        # monitor the state in the manager.
+        
+        self._timer = QTimer(self)
+        self._timer.setInterval(rdo_utils.CONSTANTS.POLL_MS)
+        self._timer.setSingleShot(False)
+        self._timer.timeout.connect(self._update)
+        self._timer.start()
+        
     # Signal handlers:
+    
+    def _update(self):
+        # Update the state from the client:
+        
+        
+        self._model.setActualTitle(self._client.title())
+        self._model.setActualRun(self._client.run())
+        
     
     def _updateTitle(self, title:str) -> None:
         # Update the title in the server and model:
