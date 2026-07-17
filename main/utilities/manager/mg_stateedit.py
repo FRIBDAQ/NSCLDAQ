042184a13d344376ea666ebee96d30e11cc02398
Ron Fox <fox@frib.msu.edu>
2026-07-17 10:22:00 -0400

    Issue #505 Manager code from Tcl -> Python
    Got clicking in the state list to do what it should do.

--- main/utilities/manager/mg_stateedit.py
+++ main/utilities/manager/mg_stateedit.py
@@ -21,18 +21,12 @@ control your experiment, this is not recommended.  Editing the
 state machine means adding and removing states as well as defining
 legals state transitions.  Legal state transitions are defined
 
-Note that the state machine is defined as a list of dicts.  The dicts have keys:
 
-* name - name of the state.
-* precursors - List of names of the legal precursor states.
-* successors - list of names of the legal successor states
-
-A precursor state is one with a legal transition to the state in 'name'.
-A successor state is one that 'name' can transition to.
 
 '''
 
-from PyQt6.QtWidgets import (QListWidget, QLabel, QLineEdit, QPushButton, QComboBox,
+from PyQt6.QtWidgets import (QListWidget, QListWidgetItem, 
+            QLabel, QLineEdit, QPushButton, QComboBox,
             QVBoxLayout, QHBoxLayout, QWidget)
 from PyQt6.QtCore import QObject
 
@@ -69,6 +63,15 @@ class StateEditor(QWidget):
         Note, the signals don't automatically run the methods associated with them.  That's left
         for the signal handler to do...as after all, databgase manipulations to do that may fail.
         
+        Note that the state machine is defined as a list of dicts.  The dicts have keys:
+
+            * name - name of the state.
+            * precursors - List of names of the legal precursor states.
+            * successors - list of names of the legal successor states
+
+            A precursor state is one with a legal transition to the state in 'name'.
+            A successor state is one that 'name' can transition to.
+        
     '''
     def __init__(self, parent : QObject | None = None):
         super().__init__(parent)
@@ -76,16 +79,82 @@ class StateEditor(QWidget):
         # the main layout is left to right for each of the
         # prececessor, state and successor regions.
         
-        self._layout = QHBoxLayout(self)
-        
-        
+        self._layout = QHBoxLayout(self)  
         
         self._layout.addLayout(self._createPrecursorWidgets())
         self._layout.addLayout(self._createStateWidgets())
         self._layout.addLayout(self._createSuccessorWidgets())
         
         self.setLayout(self._layout)
+        
+        self._stateModel = list()                   # Start with an empty model.
+        
+        
+        # Connect Signals to internal slots:
+        
+        self._stateList.itemClicked.connect(self._setPrecursors)
+        self._stateList.itemClicked.connect(self._setSuccessors)
+        self._stateList.itemClicked.connect(self._loadStateLabel)
     
+    # Attributes implementations.
+    
+    def stateMachine(self) -> list[dict]:
+        ''' 
+        @return list[dict] - return the internal model of the state machine.
+           See the class docstrings for the form of this list of dicts.
+        '''
+        return self._stateModel
+    
+    def setStateMachine(self, stateMachine : list[dict]) -> None:
+        '''
+        Set the contents of the internal model of the state machine.
+        
+        @param stateMachine - the new internal model of the state machine to use.
+            This is a list of state definitions as described in the class docstrings.
+        '''
+        self._stateModel = stateMachine
+
+        # Now load the state list, and signal the current row was clicked  to 
+        # Load the precursor and successor list boxes.
+        # Clear the selected labels in all list boxes.
+        
+        self._stockListBox(self._stateList, self._enumerateStates())
+    
+    # Internal slots:
+    
+    
+    #  These methods handle clicks in the state list:
+    
+    def _loadStateLabel(self, item : QListWidgetItem) -> None:
+        #  Load the state label with the selecte item:
+        
+        self._selectedState.setText(item.text())
+    
+    def _setPrecursors(self, item : QListWidgetItem) -> None:
+        self._setTransitionStates(item, 'precursors', self._precursorWidgets)
+        
+    def _setSuccessors(self, item :QListWidgetItem) -> None:
+        self._setTransitionStates(item, 'successors', self._successorWidgets)
+    
+    def _setTransitionStates(self, item, which, whichWidgets):
+        #  Set the appropriate transition region
+        #  which - is which key in the state machine to use to get the states
+        #  whichWidgets is the map of widgets to stock:
+        # This is commmon code for setPrecursors and setSuccessors.
+        
+        current_state = item.text()
+        state_info    = self._findState(current_state)
+        transitions    = state_info[which]
+        self._stockListBox(whichWidgets['list'], transitions)
+        
+        # Figure out which states belong in the combobox:
+        # All states but the ones alread in precursors;
+        
+        states = self._enumerateStates()
+        selectable_states = [x for x in states if x not in transitions]
+        self._stockComboBox(whichWidgets['statelist'], selectable_states)
+        
+        
     #  GUI layout utilities:
     
     def _createPrecursorWidgets(self) -> QVBoxLayout:
@@ -180,8 +249,33 @@ class StateEditor(QWidget):
         addLayout.addWidget(widgetMap['add'])
         
         layout.addLayout(addLayout)
+    
+    # Utility methods:
+    
+    def _findState(self, name : str) -> dict[str, list[str]]:
+        # Locate the dict with the specified name.
+        
+        return [x for x in self._stateModel if x['name'] == name][0]
+    
+    def _stockListBox(self, widget : QListWidget, items : list[str]) -> None:
+        # Stock a list box widget with the items passesd in.
+        # The first item is seleted and 'clicked'.
+        widget.clear()
+        widget.addItems(items)
         
         
+        if len(items):
+            widget.setCurrentRow(0)
+            widget.itemClicked.emit(widget.item(0))
+    
+    def _stockComboBox(self, widget : QComboBox, items) -> None:
+        # Stock the items that can be selected in a combobox:
+        
+        widget.clear()
+        widget.addItems(items)
+        
+    def _enumerateStates(self) -> list[str]:
+        return   [x['name'] for x in self._stateModel]      
 # Test code for now:
 
 if __name__ == '__main__':
@@ -191,6 +285,13 @@ if __name__ == '__main__':
     app = QApplication (sys.argv)
     win = StateEditor()
     
+    dummy_statemachine = [
+        {'name': 'SHUTDOWN', 'precursors': ['SHUTDOWN', 'BOOT', 'HWINIT'], 'successors' : ['BOOT',]}, 
+        {'name': 'BOOT',     'precursors' : ['SHUTDOWN',], 'successors': ['HWINIT', 'SHUTDOWN'] },
+        {'name': "HWINIT",   'precursors' : ['BOOT',], 'successors' : ['SHUTDOWN']},
+    ]
+    
+    win.setStateMachine(dummy_statemachine)
     
     win.show()
     sys.exit(app.exec())
