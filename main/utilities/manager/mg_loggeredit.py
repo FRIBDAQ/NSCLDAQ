a87c60437d4200cffdc039bfc0b1dcb1ec3e178d
Ron Fox <fox@frib.msu.edu>
2026-07-23 08:53:41 -0400

    Issue #505 Manager code from Tcl -> Python
    * Laid out LoggerDescription widget - allows a description of a logger
      to be entered.
    * Made the attributes work (at least the settings).
    * Made the external (accept) signal work.
    * Stubbed out the browse buttons.

--- main/utilities/manager/mg_loggeredit.py
+++ main/utilities/manager/mg_loggeredit.py
@@ -30,9 +30,9 @@ Event loggers have the following properties:
 '''
 
 from PyQt6.QtWidgets import (QTableView, QLabel, QLineEdit, QComboBox, QPushButton, QCheckBox,
-    QVBoxLayout, QHBoxLayout)
+    QVBoxLayout, QHBoxLayout, QWidget)
 from PyQt6.QtGui import QStandardItemModel, QStandardItem
-from PyQt6.QtCore import pyqtSignal, QObject, QModelIndex
+from PyQt6.QtCore import pyqtSignal, Qt, QObject, QModelIndex
 
 
 class LoggerTable(QTableView):
@@ -148,7 +148,244 @@ class LoggerTable(QTableView):
     def _indicatorBool(self, item : QStandardItemModel) -> bool:
         return True if item.text() == 'X' else False
         
+class  LoggerDescription(QWidget):
+    ''''
+    This compound widget contains the controls needed to specify
+    a logger.  This can be used either to specify a new logger or
+    edit an existing logger.  
+    
+    Attributes are:
+    
+    daqroot - Roo of the FRIB/NSCLDAQ directory tree to use.
+    destination - A destination directory for the logged data.
+    ring    - A ring buffer from which to take data.
+    containers - List of containers to choose from.
+    container - The selected container.
+    host     - Host in which the logger runs.
+    critical - bool - True if the logger is critical.
+    partial  - bool - true if the logger is a partial one.
+    enabled  - bool - true if the logger is enabled.
+    
+    Methods:
+    
+    setLogger - Given a logger definition dict (see LoggerTable class), fills the attributes of
+             the form.
+    
+    Signals:
+        accept - the Add/Modify button is clicked.
         
+    Note there are internal/autonomously handled signal/slots. I name those slots below.
+    
+    _browseRoot - browse for daqroot directory.
+    _browseDest - Browse for a destination directory.
+    _clear      - Clear the form.
+    '''
+    accept = pyqtSignal()
+    
+    def __init__(self, parent : QObject |None = None):
+        super().__init__(parent)
+        
+        # The overal layout is a vertical stack of horiztonal strips of widgets:
+        
+        self._layout = QVBoxLayout(self)
+        
+        #  The first horizontal strip is run-time environment.
+        
+        self._layout.addLayout(self._makeRuntimeEnv())
+        
+        # The second horizontal strip is the sourcde/dest specifiers
+        
+        self._layout.addLayout(self._makeSourceDest())
+        
+        # Next to the bottom are the checkbuttons:
+        
+        self._layout.addLayout(self._makeCheckBoxes())
+        
+        # The bottom horizontal strip is the clear and Add/Modify buttons.
+        # note that _makeButtons connects the buttons to appropriate
+        # signal handlers.
+        
+        self._layout.addLayout(self._makeButtons())
+
+        # Finalize the GU.
+        
+        self.setLayout(self._layout)
+        
+    # Implement attributes
+    
+    def daqroot(self) -> str:
+        ''' @return str - the contents of the DAQROOT line edit'''
+        return self._daqroot.text()
+    def setDaqroot(self, root : str) -> None:
+        ''' @param root : str - the new value of the DAQRoot. '''
+        self._daqroot.setText(root)
+        
+    def destination(self) -> str:
+        ''' @return str - the contents of the destination line edit. '''
+        return self._destination.text()
+    def setDestination(self, dest : str) -> None:
+        ''' @param dest: str -the new value for the destination line edit contents '''
+        self._destination.setText(dest)
+        
+    def ring(self) -> str:
+        ''' @return str - The contents of the data source ringbuffer URI'''
+        return self._ring.text()
+    def setRing(self, ring : str) -> str:
+        ''' @param ring: str - The URI of the ring to load into the destination text edit.'''
+        
+        self._ring.setText(ring)
+        
+    def containers(self) -> list[str]:
+        ''' @return list[str] - The list of containers that are valid in the combobox.'''
+        
+        return [self._container.itemText(i) for i in range(self._container.count())]
+    def setContainers(self, containers : list[str]) -> None:
+        ''' @param containers: list[str] the set of containers to load into the combobox list.'''
+        self._container.clear()
+        for container in containers:
+            self._container.addItem(container)
+    
+    def container(self) -> str:
+        ''' @return str - the currently selected container. '''
+        return self._container.currentText()
+    def setContainer(self, container: str) -> None:
+        ''' 
+            @param container : str The containre to select. 
+            @throw ValuError If cotainer that's not in the list
+        '''
+        if container not in self.containers():
+            raise ValueError(f'{container} is not in the list of known containers')
+        self._container.setCurrentText(container)
+        
+    def host(self) -> str: 
+        ''' @return str - the current value of the host line edit'''
+        return self._host.text()
+    def setHost(self, host : str) -> None:
+        ''' @param host: str - the host to put in the host line edit'''
+        self._host.setText(host)
+        
+    def critical(self) -> bool:
+        ''' @return bool state of the critical checkbox '''
+        return self._checkboxValue(self._critical)
+    def setCritical(self, state: bool) -> None:
+        ''' @param state : bool - state to set the critical check box.'''
+        self._setCheckbox(self._critical, state)
+
+    def partial(self) -> bool:
+        ''' @return bool -state of the partial checkbox. '''
+        return self._checkboxValue(self._partial)
+    def setPartial(self, value : bool) -> None:
+        ''' @param value: bool - the state of the partial check box'''
+        self._setCheckbox(self._partial, value)
+    
+    def enabled(self) -> bool:
+        ''' @return bool - the state of the enabled checkbox '''
+        return self._checkboxValue(self._enabled)
+    def setEnabled(self, value: bool) -> None:
+        ''' @param value: bool - state to set the enabled checkbox.'''
+        self._setCheckbox(self._enabled, value)
+    
+    # Local/private slots:
+
+    def _browseRoot(self) -> None:
+        #  Browse for a directory to put in the self._daqroot QLineEdit
+        pass
+
+    def _browseDest(self) -> None:
+        # Browse for a destination directory
+        pass
+
+    def _clear(self) -> None:
+        # Clear all the settable controls.
+        pass
+
+    # private GUI utility methods:
+
+    def _makeRuntimeEnv(self) -> QHBoxLayout:
+        #  Create the labels and controls for the loggers's runtime
+        #  environment. These are put in a horiztonal strip (QHBoxLayout) and
+        #  the layout returned to the caller to be put in the top level layout.
+        
+        layout = QHBoxLayout()
+        layout.addWidget(QLabel('DAQROOT', self))
+        
+        self._daqroot = QLineEdit(self)
+        layout.addWidget(self._daqroot)
+        self._browseRootButton = QPushButton('Browse...', self)
+        layout.addWidget(self._browseRootButton)
+        self._browseRootButton.clicked.connect(self._browseRoot)
+        
+        layout.addWidget(QLabel('Container', self))
+        self._container = QComboBox(self)
+        layout.addWidget(self._container)
+        
+        layout.addWidget(QLabel('Host', self))
+        self._host = QLineEdit(self)
+        layout.addWidget(self._host)
+        
+        return layout
+
+    def _makeSourceDest(self) -> QHBoxLayout:
+        #  Return a horiztonal layout that contains the
+        #  labels and control to set a logger's source
+        #  and destination.
+        
+        layout = QHBoxLayout()
+        
+        layout.addWidget(QLabel('Ring URI', self))
+        self._ring = QLineEdit(self)
+        layout.addWidget(self._ring)
+        
+        layout.addWidget(QLabel('Destination', self))
+        self._destination = QLineEdit(self)
+        layout.addWidget(self._destination)
+        self._browseDestinationButton = QPushButton('Browse...')
+        layout.addWidget(self._browseDestinationButton)
+        self._browseDestinationButton.clicked.connect(self._browseDest)
+
+        return layout
+
+    def _makeCheckBoxes(self) -> QHBoxLayout:
+        #  The checkboxes for the bool values:
+        layout = QHBoxLayout()
+        
+        self._critical = QCheckBox('Critical', self)
+        layout.addWidget(self._critical)
+        
+        self._partial = QCheckBox('Partial', self)
+        layout.addWidget(self._partial)
+        
+        self._enabled = QCheckBox('Enabled', self)
+        layout.addWidget(self._enabled)
+        
+        return layout
+    def _makeButtons(self) -> QHBoxLayout:
+        # Return a layout that contains the clear and Add/modify buttons.
+        # Note that signals are hooked up
+        
+        layout = QHBoxLayout()
+        
+        self._clearButton = QPushButton('Clear', self)    
+        layout.addWidget(self._clearButton)
+        self._clearButton.clicked.connect(self._clear)
+        
+        self._acceptButton = QPushButton('Add/Modify', self)
+        layout.addWidget(self._acceptButton)
+        self._acceptButton.clicked.connect(self.accept)
+        
+        return layout
+    
+    # Utlities:
+    
+    def _checkboxValue(self, box : QCheckBox) -> bool:
+        # return checkbox value as a bool:
+        
+        return box.checkState() == Qt.CheckState.Checked
+    
+    def _setCheckbox(self, box : QCheckBox, value: bool):
+        box.setCheckState(
+            Qt.CheckState.Checked if value else Qt.CheckState.Unchecked
+        )
 # Test code for now
 
 if __name__ == '__main__':
@@ -158,7 +395,9 @@ if __name__ == '__main__':
     def dbl(logger : dict) -> None:
         print(logger)
         
-        
+    def accept() -> None:
+        print('accepted'
+              )
     app = QApplication(sys.argv)
     win = LoggerTable()
     
@@ -174,4 +413,20 @@ if __name__ == '__main__':
     
     win.loggerSelected.connect(dbl)
     win.show()
+    
+    # Edit widget:
+    
+    edit = LoggerDescription()
+    edit.setDaqroot('/usr/opt/daq/12.2-009')
+    edit.setDestination('/events/ron')
+    edit.setRing('tcp://localhost/ron')
+    edit.setContainers(['jessie', 'buster', 'bullseye', 'bookworm'])
+    edit.setContainer('bullseye')
+    edit.setHost('spdaq20.frib.msu.edu')
+    edit.setCritical(True)
+    edit.setPartial(False)
+    edit.setEnabled(True)
+    edit.show()
+    edit.accept.connect(accept)
+    
     sys.exit(app.exec())
