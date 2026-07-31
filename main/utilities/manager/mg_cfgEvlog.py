309e2f6d1cd6988598c60bebd04c86807e411b62
Ron Fox <fox@frib.msu.edu>
2026-07-31 08:08:25 -0400

    Issue #505 Manager code from Tcl -> Python
    Wrote a table view with integrated model for to list event
    log tables that can support selection of rows.

---
+++ main/utilities/manager/mg_cfgEvlog.py
@@ -0,0 +1,208 @@
+#/usr/bin/env python3
+
+#    This software is Copyright by the Board of Trustees of Michigan
+#    State University (c) Copyright 2014, 2026
+#
+#    You may use this software under the terms of the GNU public license
+#    (GPL).  The terms of this license are described at:
+#
+#     http://www.gnu.org/licenses/gpl.txt
+#
+#	     FRIB
+#	     Michigan State University
+#	     East Lansing, MI 48824-1321
+
+
+'''
+Configuration program for event loggers.  See the event log
+wizard for a simpler way to get started.
+
+@file mg_cfgEvlog.py
+@brief Configure event loggers.
+@author Ron Fox.
+@note  The loggers are represented for the most part by a dict which has the following keys:
+* root - DAQ Root describing, along with the container which event log to run.
+* ring - Source of data to be logged.
+* host - Host the logger runs in.
+* partial - If true, the logger is like a mutlilogger just making a soup of event files, otherwise 
+         maintains the full directory structure.
+* destination - Where the data are logged, should be an existing directory by the time the
+        logger starts.
+* critical - True if the experiment should be shutdown if the logger unexpectedly exits.
+* enabled - True if the logger should log when event logging is turned on.
+* container - Container in which the logger runs.
+'''
+
+from PyQt6.QtWidgets import QTableView
+from PyQt6.QtGui     import QStandardItemModel, QStandardItem
+from PyQt6.QtCore    import pyqtSignal, QObject, QModelIndex
+class EventLogTable(QTableView):
+    '''
+        This is a table view with integrated model for listing
+        event loggers and their properties. 
+        
+        Properties:
+            loggers - see the module docstrings for informationa bout
+                     the format these are in.
+        Methods:
+            updateRow - Update a row to the new dict.
+            deleteRow - Delete a row in the model.
+        Signals:
+            selected(dict) - A logger was double clicked somewhere.  The 
+                    dict describing the logger is passed to the slot.
+                
+    '''
+    selected = pyqtSignal(dict)
+    def __init__(self, parent : QObject | None = None):
+        super().__init__(parent)
+        
+        
+        self.setModel(QStandardItemModel())
+        self._clear()           # Sets up the column headers.
+        
+        # Handle the doubleClicked signal and turn it into selected:
+        
+        self.doubleClicked.connect(self._doubleClickRelay)
+    
+    # Implement attributes:
+    
+    def loggers(self) -> list[dict]:
+        '''
+        @return list[dict] - list of logger definitions in the model.
+        '''
+        result = list()
+        for row in range(self.model().rowCount()):
+            result.append(self._rowToDict(row))
+        
+        return result
+    
+    def setLoggers(self, loggers : list[dict]) -> None:
+        '''
+        @param loggers : list[dict] - list of logger definition dicts to load
+            into the table.  All previously defined loggers are removed.
+        '''
+        self._clear()
+        for logger in loggers:
+            self.model().appendRow(self._dictToRow(logger))
+    
+        self.resizeColumnsToContents()
+        self.verticalHeader().hide()
+    # Public methods:
+    
+    def updateRow(self, olddest: str, logger: dict) -> None:
+        '''
+            Given a row with the specified destination, update it
+            to match the new logger definition (this can change the
+            destination too).
+            
+            @param olddest :str - the destination in the row to change.
+            @param logger  : dict - new logger definition to laod in that row.
+        '''
+        m = self.model()
+        row = self._findDestination(olddest)
+        rowValues = self._dictToRow(logger)
+        
+        for col, item in enumerate(rowValues):
+            m.setItem(row, col, item)
+    
+    def deleteRow(self, destination: str) -> None:
+        '''
+            @param destination : str - the logger destination row to delete.
+        '''
+        row = self._findDestination(destination)
+        self.model().takeRow(row)
+        
+    # Internal/private slots:
+    
+    def _doubleClickRelay(self, index :QModelIndex) -> None:
+        row = index.row()
+        logger = self._rowToDict(row)
+        self.selected.emit(logger)
+    # Utility methods:
+    #
+    def _dictToRow(self, logger : dict) -> list[QStandardItem]:
+        # Utility to turn a logger dict into the list of standard items that
+        # fit in a row:
+        
+        root        = QStandardItem(logger['root'])
+        source      = QStandardItem(logger['ring'])
+        host        = QStandardItem(logger['host'])
+        partial     = self._boolToItem(logger['partial'])
+        destination = QStandardItem(logger['destination'])
+        critical    = self._boolToItem(logger['critical'])
+        enabled     = self._boolToItem(logger['enabled'])
+        container   = QStandardItem(logger['container'])
+        
+        return [
+            root, source, host, destination, container, partial, critical, enabled
+        ]
+    
+    def _rowToDict(self, row : int) -> dict:
+        # Return the dict that corresponds to the row specified in the model:
+        
+        m = self.model()      # For brevity.
+        root = m.item(row, 0).text()
+        source = m.item(row, 1).text()
+        host  = m.item(row,2).text()
+        destination = m.item(row, 3).text()
+        container   = m.item(row, 4).text()
+        partial     = self._itemToBool(m.item(row,5))
+        critical    = self._itemToBool(m.item(row, 6))
+        enabled     = self._itemToBool(m.item(row, 7))
+        return {
+            'root': root, 'ring': source, 
+            'host': host, 'partial': partial, 'destination' : destination,
+            'critical' : critical, 'enabled': enabled
+        }
+        
+    def _clear(self) -> None:
+        self.model().clear()
+        self.model().setHorizontalHeaderLabels(["DAQ Root", "Source", "Host", "Destination", "Container", "P", "C", "E"])
+        
+    def _boolToItem(self, value : bool) -> QStandardItem:
+        return QStandardItem('X' if value else ' ')
+    
+    def _itemToBool(self, item : QStandardItem) -> bool:
+        return True if item.text() == 'X' else False
+    
+    def _findDestination(self, dest) -> int:
+        #  Return the row that has the specified destination or IndexError if no such row.
+        
+        m = self.model()
+        matches = m.findItems(dest, 3)  # Detination column.
+        if len(matches) == 0:
+            raise IndexError(f'There is no row with the destination {oddest}')
+        
+        row = matches[0].row()
+        return row
+    
+# Test code for now:
+
+if __name__ == "__main__":
+    from PyQt6.QtWidgets import QApplication
+    import sys
+
+    app = QApplication(sys.argv)
+    win = EventLogTable()
+    
+    def sel(logger: dict) -> None:
+        print(logger)
+    
+    someloggers = [
+        {
+            'root' : '/usr/opt/daq/12.2-009', 'ring' : 'tcp://localhost/ron', 
+            'host' : 'localhost', 'partial' : True, 
+            'destination' : '/home/ron/stagearea/partial', 'critical' : False, 'enabled' : True,
+            'container' : 'bookworm'
+        },
+        {
+            'root' : '/usr/opt/daq/12.2-009', 'ring' : 'tcp://localhost/built', 
+            'host' : 'localhost', 'partial' : False, 
+            'destination' : '/home/ron/stagearea/built', 'critical' : True, 'enabled' : True,
+            'container' : 'bookworm'
+        }
+    ]
+    win.setLoggers(someloggers)
+    win.selected.connect(sel)
+    win.show()
+    sys.exit(app.exec())
