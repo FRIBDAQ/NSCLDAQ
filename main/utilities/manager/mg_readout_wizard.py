9c76e7a79f1af9e14f76c8c67a244022d7774121
Ron Fox <fox@frib.msu.edu>
2026-08-03 10:02:17 -0400

    Issue #505 Manager code from Tcl -> Python
    Laid out the common parmaeters part of the wizard..got all
    the fields it has working too.
    all fields working.

--- main/utilities/manager/mg_readout_wizard.py
+++ main/utilities/manager/mg_readout_wizard.py
@@ -23,14 +23,18 @@ have to run.
 '''
 
 
-from PyQt6.QtWidgets import QWizard, QWizardPage, QTextEdit, QVBoxLayout
+from PyQt6.QtWidgets import (QWizard, QWizardPage, QTextEdit, QVBoxLayout, QLineEdit, 
+    QComboBox, QPushButton, QLabel, QHBoxLayout, QFileDialog
+)
 from PyQt6.QtCore    import QObject, pyqtSignal
+import getpass
 
 class IntroPage(QWizardPage):
     '''
         Just provides introductory text.
         This will be registered with page id 1 and always
         branches to page id 2.
+        
     '''
     
     def __init__(self, parent : QObject | None = None):
@@ -73,12 +77,158 @@ Click the next button when you are ready to continue.
         self.setTitle("Introduction:")
     def nextid(self) -> int:
         ''' stub for now:'''
-        return -1
+        return 2
     
     def  pageId(self) -> int:
         '''' All our wizard pages will know their own page id and next id.'''   
         return 1
         
+class CommonReadoutInfo(QWizardPage):
+    ''' 
+        There's a lot of common information that does not
+        change from Readout type to readout type.  This is prompted
+        for on this page, along with the Readout type. Our nextid will
+        wind up depending on the value of the ReadoutType combobox.
+        Fields:
+            Name      - name of the readout program.
+            Container - The Container to use.
+            Host      - Host in which to run the programs.
+            Directory - The CWD for the programs.
+            RestService - The ReST service advertised by the Readout.
+            RingBuffer - Name of the ring buffer Readout writes data to.
+            SourceId   - Source id.
+            User       - User that will run the manager/Readout.
+            ReadoutType - The type of readout  that will be run (determines the next page id
+                          See the ReadoutWizard docstring).
+                          
+        Attributes which the main wizard will need to expose in some way:
+            containers - Valid container names.
+                
+            We'll try to set the combobox fields to be the current text rather than current index.
+    '''
+    def __init__(self, parent : QObject | None = None):
+        super().__init__(parent)
+        self._containerlist = []
+        
+    def initializePage(self) -> None:
+        self.setTitle('Common parameters')
+        self._layout = QVBoxLayout(self)
+        self.setLayout(self._layout)   # Our usual vertical stack of strips.
+        
+        # Name of readout (Name Field):
+        
+        nameLayout = QHBoxLayout()
+        nameLayout.addWidget(QLabel('Readout Name:', self))
+        self._name = QLineEdit(self)
+        self.registerField('Name', self._name)
+        nameLayout.addWidget(self._name)
+        
+        self._layout.addLayout(nameLayout)
+        
+        # Container, host and cwd (browsable).
+        
+        environLayout = QHBoxLayout()
+        environLayout.addWidget(QLabel('Container', self))
+        self._container = QComboBox(self)
+        self._container.addItems(self._containerlist)
+        self.registerField('Container', self._container, 'currentText', QComboBox.currentTextChanged)
+        environLayout.addWidget(self._container)
+        
+        environLayout.addWidget(QLabel('Host', self))
+        self._host = QLineEdit(self)
+        self._host.setText('            ')
+        self.registerField('Host', self._host)
+        environLayout.addWidget(self._host)
+        
+        environLayout.addWidget(QLabel('Working Dir', self))
+        self._wd = QLineEdit(self)
+        self._wd.setText('            ')
+        self.registerField('Directory', self._wd)
+        environLayout.addWidget(self._wd)
+        self._browseWd = QPushButton('Browse...', self)
+        environLayout.addWidget(self._browseWd)
+        
+        self._layout.addLayout(environLayout)
+        
+        #  How the Readout outputs data:
+        
+        outputLayout = QHBoxLayout()
+        outputLayout.addWidget(QLabel('Output Ring', self))
+        self._ring = QLineEdit(self)
+        self.registerField('RingBufer', self._ring)
+        self._ring.setText(getpass.getuser())
+        outputLayout.addWidget(self._ring)
+        
+        outputLayout.addWidget(QLabel('Source ID', self))
+        self._sourceid = QLineEdit(self)
+        self.registerField('SourceId', self._sourceid)
+        outputLayout.addWidget(self._sourceid)
+        
+        self._layout.addLayout(outputLayout)
+        
+        # ReST service information:
+        
+        restLayout  = QHBoxLayout()
+        restLayout.addWidget(QLabel('ReST service', self))
+        self._service = QLineEdit(self)
+        self.registerField('RestService', self._service)
+        self._service.setText('RedoutREST')
+        restLayout.addWidget(self._service)
+        
+        restLayout.addWidget(QLabel('Manager username', self))
+        self._user = QLineEdit(self)
+        self.registerField('User', self._user)
+        self._user.setText(getpass.getuser())
+        restLayout.addWidget(self._user)
+        
+        self._layout.addLayout(restLayout)
+        
+        #  Finally the combobox with the Readout types:
+        
+        typeLayout = QHBoxLayout()
+        typeLayout.addWidget(QLabel('Readout Type', self))
+        self._readoutType = QComboBox(self)
+        self.registerField('ReadoutType', self._readoutType, 'currentText', QComboBox.currentTextChanged)
+        self._readoutType.addItems(['XIA/DDAS', 'VMUSB', 'MVLC', 'CCUSB', 'Custom'])
+        typeLayout.addWidget(self._readoutType)
+        
+        self._layout.addLayout(typeLayout)
+        
+        
+        # The browse button needs to browse directories:
+        
+        self._browseWd.clicked.connect(self._browseDir)
+    # Attribute implementations:
+    
+    def containers(self) -> list[str]:
+        ''' Return the containers in the combobox.'''    
+       
+        return self._containerlist
+    
+    def setContainers(self, containers : list[str]) -> None:
+        self._containerlist = containers
+    
+    # Page navigation:
+    
+    def nextid(self) -> int:
+        # The next page id depends on the value of the 'ReadoutType field.
+        match self.wizard().field('ReadoutType'):
+            case 'XIA/DDAS':
+                return 100
+            case _:
+                return -1
+            
+    def pageId(self) -> int:
+        return 2
+    
+    # Internal (private) slots:
+    
+    def _browseDir(self) -> None:
+        # Browse for the home directory.
+        
+        dir = QFileDialog.getExistingDirectory(self, 'Choose Working Directory')
+        if dir.strip():
+            self._wd.setText(dir)
 class ReadoutWizard(QWizard):
     '''
         Readout configuration wizard.  Note that this wizard
@@ -89,6 +239,9 @@ class ReadoutWizard(QWizard):
         Page ids starting at 400 are custom readouts.
         
         Page ids below that are common initial pages
+        
+        Attributes:
+        containers - List of containers the user can choose from.
     '''
     def __init__(self, parent : QObject | None = None):
         super().__init__(parent)
@@ -101,7 +254,15 @@ class ReadoutWizard(QWizard):
         self.setPage(self._intro.pageId(), self._intro)
         self.setStartId(self._intro.pageId())
         
+        self._commonInfo = CommonReadoutInfo(self)
+        self.setPage(self._commonInfo.pageId(), self._commonInfo)
         
+    def containers(self) -> list[str]:
+        ''' @return list[str] - list of containers that are available.'''
+        return self._commonInfo.containers()
+    def setContainers(self, containers : list[str]) -> None:
+        self._commonInfo.setContainers(containers)
+    
 
 ##  Test code for now:
 
@@ -111,10 +272,20 @@ if __name__ == "__main__":
 
     def done():
         print("accepted:")
+        print('Name', wiz.field('Name'))
+        print('Container', wiz.field('Container'))
+        print('Host', wiz.field('Host'))
+        print('Directory', wiz.field('Directory'))
+        print('Service', wiz.field('RestService'))
+        print('Ring', wiz.field('User'))
+        print('SrcId', wiz.field('SourceId'))
+        print('User', wiz.field('User'))
+        print('Type: ', wiz.field('ReadoutType'))
         
     app = QApplication(sys.argv)
     wiz = ReadoutWizard()
     wiz.accepted.connect(done)
+    wiz.setContainers(['bookworm', 'bullseye', 'buster', 'jessie'])
     wiz.show()
     
     sys.exit(app.exec())
