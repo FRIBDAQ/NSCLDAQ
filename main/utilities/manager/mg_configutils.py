ed2b5c93801e47b4996c55f08cf21ff5a81b3129
Ron Fox <fox@frib.msu.edu>
2026-08-04 10:58:24 -0400

    Issue #505 Manager code from Tcl -> Python
    Factor out EditableTable from the program wizard, and port it to qt6

--- main/utilities/manager/mg_configutils.py
+++ main/utilities/manager/mg_configutils.py
@@ -13,8 +13,20 @@
 ''' Module with configuration utilities.
 
 '''
-from PyQt6.QtWidgets import QDialog, QDialogButtonBox, QWidget, QVBoxLayout
-from PyQt6.QtCore    import QObject
+from PyQt6.QtCore import QObject
+from PyQt6.QtWidgets import (
+    QDialog,
+    QDialogButtonBox,
+    QHBoxLayout,
+    QPushButton,
+    QStyle,
+    QTableWidget,
+    QTableWidgetItem,
+    QVBoxLayout,
+    QWidget,
+)
+
+
 class SaveDialog(QDialog):
     '''
         This is a dialog with a button box that has save and cancel buttons.
@@ -48,3 +60,94 @@ class SaveDialog(QDialog):
     def keyPressEvent(self, event) -> None:
         pass
     
+
+
+class EditableTable(QWidget):
+    '''  Megawidget that has a table that can be edited.
+      Editing consists of supported deletion of the current row,
+      Adding rows to the end, and editing the entries.
+     
+      Layout is:
+       
+        +--------------------------+
+        |    The table             | [+]
+        |                          | [x]
+        +--------------------------+
+    
+       The table() method returns the table widget.
+       this allows for things like setting the headers, the # of colomns
+       loading it etc.
+    '''
+    def __init__(self, *args):
+        super().__init__(*args)
+        layout = QHBoxLayout()
+        
+        self._table = QTableWidget(self)
+        defaultItem = QTableWidgetItem('')
+        self._table.setItemPrototype(defaultItem)
+        layout.addWidget(self._table)
+        
+        #  The buttons:
+        
+        buttonlayout = QVBoxLayout()
+        self._addrow = QPushButton('+', self)
+        buttonlayout.addWidget(self._addrow)
+        self._deleterow = QPushButton(self)
+        self._deleterow.setIcon(self.style().standardIcon(getattr(QStyle.StandardPixmap, 'SP_DialogDiscardButton')))
+        buttonlayout.addWidget(self._deleterow)
+        
+        
+        layout.addLayout(buttonlayout)
+        
+        self.setLayout(layout)    
+        self._addrow.clicked.connect(self._addRow)
+        self._deleterow.clicked.connect(self._deleteRow)
+        
+    def _addRow(self):
+        self._table.setRowCount(self._table.rowCount() + 1)
+        new_row = self._table.rowCount() - 1
+        cols = self._table.columnCount()
+        for c in range(cols):
+            self._table.setItem(new_row, c, self._table.itemPrototype().clone())
+    def _deleteRow(self):
+        row = self._table.currentRow()
+        self._table.removeRow(row)
+        
+    def table(self):
+        ''' Returns the table itself. '''
+        return self._table
+    def col0List(self):
+        ''' Returns a list of the values in column 0 with empties suppressed: '''
+        
+        table = self._table
+        rows  = table.rowCount()
+        return [table.item(r, 0).text()
+                for r in range(rows)
+                if len(table.item(r,0).text()) > 0 
+                and not table.item(r, 0).text().isspace()]
+
+    def getPairs(self):
+        '''
+           Return a list of col0, col1 pairs with
+           Elements that have no col0 value omitted.
+           The result is a list of two element lists for cases
+           when both columns are filled in and one element lists if the
+           col1 is not filled in.
+        '''
+        
+        table = self._table
+        rows = table.rowCount()
+        result = []
+        
+        rawoptions = [[table.item(r, 0).text(), table.item(r, 1).text()] 
+                      for r in range(rows)
+                      if len(table.item(r, 0).text()) > 0
+                      and not table.item(r, 0).text().isspace()
+                      ]
+        for (option, value) in rawoptions:
+            if len(value) == 0 or value.isspace():
+                result.append((option,))
+            else:
+                result.append((option, value))
+        return result
+        
