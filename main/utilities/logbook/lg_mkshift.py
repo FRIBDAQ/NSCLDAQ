dc2f534ed158df9e342c6c99505e492949082463
Ron Fox <fox@frib.msu.edu>
2026-08-21 08:32:51 -0400

    Issue #514 - port logbook utilities to Python
    Get graphical shift prompting working.

--- main/utilities/logbook/lg_mkshift.py
+++ main/utilities/logbook/lg_mkshift.py
@@ -19,6 +19,7 @@
     @author Ron Fox
 '''
 from nscldaq.LogBook import logbookadmin, LogBook
+
 import sys
 
 
@@ -32,6 +33,84 @@ def usage() -> None:
     print('If the shiftname is supplied, an empty shift is created.  The members can be', file=sys.stderr)
     print('managed with $DAQBIN/lg_mgshift', file=sys.stderr)
 
+def prompt() -> tuple[str | None, list[LogBook.Person] | None]:
+    # These imports are here so they only happen if I'm using pyqt6:
+    
+    from PyQt6.QtWidgets import (QApplication, QDialog, QDialogButtonBox, QLabel, QLineEdit,
+                                 QHBoxLayout, QVBoxLayout, QMessageBox)
+    from nscldaq.LogBook.LogBookUIUtilities import ShiftMemberEditor
+    
+    class ShiftCreationDialog(QDialog):
+        def __init__(self):
+            super().__init__(parent = None)
+            
+            self._layout = QVBoxLayout()
+            self.setLayout(self._layout)
+            
+            # Top bit is the prompt for the shiftname and line edit for it:
+            
+            namelayout = QHBoxLayout()
+            namelayout.addWidget(QLabel('Shift Name: ', self))
+            self._name = QLineEdit(self)
+            namelayout.addWidget(self._name)
+            
+            self._layout.addLayout(namelayout)
+            
+            #  Now the shift editor:
+            
+            self._memberEditor = ShiftMemberEditor(self)
+            self._layout.addWidget(self._memberEditor)
+            
+            # The buttons and their connections:
+            
+            self._buttons = QDialogButtonBox(
+                QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel,
+                self
+            )
+            self._layout.addWidget(self._buttons)
+            self._buttons.accepted.connect(self.accept)
+            self._buttons.rejected.connect(self.reject)
+            
+        # Get the name:
+        
+        def name(self) -> str:
+            '''
+                @return str - the shfit name.
+            '''
+            
+            return self._name.text()
+        
+        
+        
+        def setPeople(self, people : list[LogBook.Person]) -> None:
+            '''
+                @param people : list[LogBook.Person] - People known to the logbook.
+            '''
+            
+            self._memberEditor.setNonMembers(people)
+        def getMembers(self) -> list[LogBook.Person]:
+            '''
+            @return list[LogBook.Person] - List of the people on the shift.
+            '''
+            return self._memberEditor.members()
+    
+    # Now we have a prompter dialog, we can start it up.  Note that we
+    # require a non-empty shift name:
+    
+    _app = QApplication(sys.argv)
+    prompt = ShiftCreationDialog()
+    prompt.setPeople(logbookadmin.listPeople())
+    
+    while True:
+        if prompt.exec() ==  QDialog.DialogCode.Accepted:
+            name = prompt.name()
+            if name.strip():  # Require a name.
+                return (name, prompt.getMembers()) 
+            else:
+                QMessageBox.warning(prompt, 'Missing data', 'The shift must have a non blank name')
+        else:
+            return (None, None)
+
 def main() -> int:
     if not logbookadmin.currentLogBook():
         print('You must use $DAQBIN/lg_current to establish the current logbook', file=sys.stderr)
@@ -41,12 +120,12 @@ def main() -> int:
         usage()
         return -1
     
-    members = []
+    members : list[LogBook.Person] = []
     
     if len(sys.argv) == 2:
         shiftname = sys.argv[1]
     else:
-        shiftname = None
+        (shiftname, members) = prompt()
         
     # If there's a valid shiftname, create it.
     
