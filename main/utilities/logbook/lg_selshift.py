2dda4047b9d48d107473bf4b5364ffea756db5fb
Ron Fox <fox@frib.msu.edu>
2026-08-25 08:26:59 -0400

    Issue #514 - port logbook utilities to Python
    Layout the full UI for the GUI version of the code.

--- main/utilities/logbook/lg_selshift.py
+++ main/utilities/logbook/lg_selshift.py
@@ -18,6 +18,21 @@
 @author Ron Fox
 '''
 import sys
+from collections.abc import Iterable
+
+from LogBookUIUtilities import ShiftChooser
+from nscldaq.LogBook import LogBook, logbookadmin
+from PyQt6.QtCore import Qt, QTimer, pyqtSignal
+from PyQt6.QtWidgets import (
+    QApplication,
+    QHBoxLayout,
+    QLabel,
+    QListWidget,
+    QPushButton,
+    QVBoxLayout,
+    QWidget,
+)
+
 
 def usage() -> None:
     print('Usage', file=sys.stderr)
@@ -27,10 +42,148 @@ def usage() -> None:
     print('    If shift-name is not supplied this is a GUI shift selector that also shows the current shift', file=sys.stderr)
     print('    and its members, updated periodically (in case another terminal changes the current shift)')
 
+class ShiftMemberList(QListWidget):
+    # Lists the members of a shift.
+    
+    def __init__(self, parent : QWidget | None = None):
+        super().__init__(parent)
+    
+    def setShift(self, shift : LogBook.Shift | None) -> None:
+        self.clear()
+        for member in shift.members:
+            member_text = f'{member.salutation} {member.firstname} {member.lastname}'
+            self.addItem(member_text)
+            
+
+class CurrentShift(QWidget):
+    # Display the current shift  and
+    # its members.
+    
+    def __init__(self, parent : None | QWidget = None):
+        super().__init__(parent)
+        self._layout = QVBoxLayout()
+        self.setLayout(self._layout)
+        
+        # Shiftname:
+        
+        shiftnameLayout = QHBoxLayout()
+        shiftnameLayout.addWidget(QLabel('Current Shift: ', self))
+        self._shiftname = QLabel('<Not Set>', self)
+        shiftnameLayout.addWidget(self._shiftname)
+        
+        self._layout.addLayout(shiftnameLayout)
+        
+        # The Shift members:
+        
+        self._layout.addWidget(QLabel('Members:', self))
+        self._members = ShiftMemberList(self)
+        self._layout.addWidget(self._members)
+        
+    def setShift(self, shift : LogBook.Shift | None) -> None:
+        self._members.setShift(shift)
+        name = "<Not set>" if not shift else shift.name
+        self._shiftname.setText(name)
+            
+
+class ShiftSelection(QWidget):
+    # Combine a ShiftChooser with a member list.
+    # 
+    shiftselected =  pyqtSignal(str)    # Combo box changed
+    shiftchosen    =  pyqtSignal(str)    # click of apply.
+    
+    def __init__(self, parent : QWidget | None = None):
+        super().__init__(parent)
+        
+        # THe combobox and list are side by side with an
+        # Apply button below:
+        
+        self._layout  = QVBoxLayout()
+        self.setLayout(self._layout)
+        
+        selectorLayout = QHBoxLayout()
+        self._selection = ShiftChooser(self)
+        selectorLayout.addWidget(self._selection, 0, Qt.AlignmentFlag.AlignTop)
+        self._selectedMembers = ShiftMemberList(self)
+        selectorLayout.addWidget(self._selectedMembers)
+        
+        self._layout.addLayout(selectorLayout)
+        
+        self._applybutton = QPushButton('Apply', self)
+        self._layout.addWidget(self._applybutton)
+        
+        #  Relay the signals as needed.
+        
+        self._selection.shiftselected.connect(self.shiftselected)   # Straight relay.
+        self._applybutton.clicked.connect(self._apply)
+    
+    # Attributes:
+    
+    def shift(self)  -> str:
+        # Selected shift name:
+        
+        self._selection.shift()
+        
+    def setShifts(self, shifts : Iterable[LogBook.Shift]) -> None:
+        shiftnames = [s.name for s in shifts]
+        self._selection.setShifts(shiftnames)
+    
+    def setShift(self, shift : LogBook.Shift) -> None:
+        self._selectedMembers.setShift(shift)
+    
+    # Local/private slots:
+    
+    def _apply(self) -> None:
+        self.shiftchosen.emit(self.shfit())
+    
+    
+class MainWidget(QWidget):
+    # Top is CurrentShift.
+    # Bottom is ShiftSelection.
+    # These widgets are fetcahble via methods:
+    
+    def __init__(self, parent : QWidget | None = None) -> None:
+        super().__init__(parent)
+        self._layout = QVBoxLayout()
+        self.setLayout(self._layout)
+        
+        self._current = CurrentShift(self)
+        self._layout.addWidget(self._current)
+        
+        self._selector  = ShiftSelection(self)
+        self._layout.addWidget(self._selector)
+        
+    # Let our client get the subwidgets:
+    
+    def current(self) -> CurrentShift:
+        return self._current
+    def selector(self) -> ShiftSelection:
+        return self._selector
+    
+    
+def Gui() -> int:
+    app = QApplication(sys.argv)
+    widget = MainWidget()
+    widget.show()
+    
+    
+    return app.exec()
+        
+    
+
 def main() -> int:
     if len(sys.argv) > 2:
         usage()
         return -1
-
+    if len(sys.argv) == 2:
+        shiftname = sys.argv[1]
+        if shiftname in logbookadmin.listShifts():
+            logbookadmin.setCurrentShift(shiftname)
+            return 0
+        else:
+            print(f'{shiftname} is not the name of a shift', file=sys.stderr)
+            return -1
+    else:
+        return Gui()
+        
 if __name__ == "__main__":
     sys.exit(main())
