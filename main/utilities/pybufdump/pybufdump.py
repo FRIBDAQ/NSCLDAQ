63c7cf53a55541aa1a76a85955a14c16d898ca32
Ron Fox <fox@frib.msu.edu>
2026-09-08 11:28:49 -0400

    Issue #529 - write python graphical buffer dumper
    Main program for the buffer dumper.

---
+++ main/utilities/pybufdump/pybufdump.py
@@ -0,0 +1,48 @@
+#!/usr/bin/env python3
+
+#    This software is Copyright by the Board of Trustees of Michigan
+#    State University (c) Copyright 2014, 2026
+#
+#    You may use this software under the terms of the GNU public license
+#    (GPL).  The terms of this license are described at:
+#
+#     http://www.gnu.org/licenses/gpl.txt
+#
+#	     FRIB
+#	     Michigan State University
+#	     East Lansing, MI 48824-1321
+
+'''
+@file pybufdump.py
+@brief Main program for the pyqt buffer dumper.
+@author Ron Fox
+'''
+import pyUI
+import pybufdumpController
+import sys
+from PyQt6.QtWidgets import QApplication
+
+
+def onExit() -> None:
+    QApplication.instance().exit(0)
+
+
+
+def main() -> int:
+    ''' Program entry point
+    
+    '''
+    app = QApplication(sys.argv)
+    win = pyUI.MainWindow()
+    win.show()
+    
+    _controller = pybufdumpController.BufDumpController(win, win)
+    
+    # Exit when requested.
+    
+    win.exit.connect(onExit)
+    
+    return app.exec()
+
+if __name__ == "__main__":
+    sys.exit(main())
